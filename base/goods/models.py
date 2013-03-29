86ddd63bee9ffc3530732e2cb6b9be4988a7ee97
Jen Veloso <jen@revyver.com>
2013-03-28 18:52:54 -0700

    Wrote models for Good, Source and Event
    Event will have its own model later.

--- base/goods/models.py
+++ base/goods/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+	name = models.CharField(max_length=200)
+	url = models.UrlField()
+	start_date = models.DateField()
+	end_date = models.DateField()
+	
+
+class Source(models.Model):
+	name = models.CharField(max_length=200)
+	url = models.UrlField(blank=True)
+
+	
+class Good(models.Model):
+	name = models.CharField(max_length=200)
+	event = models.ForeignKey(Event)
+	source = models.ForeignKey(Source)
+	available_from = models.DateField(blank=True)
+	available_until = models.DateField(blank=True)
+	
