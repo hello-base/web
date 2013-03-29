89611a40e8df5481d2248bbf8d4a5954d9799d05
Jen Veloso <jen@revyver.com>
2013-03-28 19:00:29 -0700

    Created events app
    Moving it out of goods because it's its own thing.

--- base/goods/models.py
+++ base/goods/models.py
@@ -11,12 +11,14 @@ class Event(models.Model):
 class Source(models.Model):
 	name = models.CharField(max_length=200)
 	url = models.UrlField(blank=True)
-
+	
 	
 class Good(models.Model):
 	name = models.CharField(max_length=200)
-	event = models.ForeignKey(Event)
-	source = models.ForeignKey(Source)
+	event = models.ForeignKey(Event, blank=True)
+	source = models.ForeignKey(Source, blank=True)
 	available_from = models.DateField(blank=True)
 	available_until = models.DateField(blank=True)
+	url = models.UrlFeild(blank=True)
+	image = models.ImageField(blank=True)
 	
