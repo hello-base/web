6dc627b57801dcfa5668140c2ffaf8251a6dd212
Jen Veloso <jen@revyver.com>
2013-03-28 22:09:01 -0700

    Fleshed out events model and tidied up attributes
    Convention change:
    name -> romanized_name
    kanji_name -> name

--- base/events/models.py
+++ base/events/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 class Event(models.Model):
+	romanized_name = models.CharField(max_length=200)
+	name = models.CharField(max_lenth=200)
+	url = models.URLField(blank=True)
+	start_date = models.DateField(blank=True)
+	end_date = models.DateField(blank=True)
+	performances = models.ForeignKey(Performance)
+
+
+class Performance(models.Model):
+	day = models.DateField()
+	start_time = models.TimeField(blank=True)
+	end_time = models.TimeField(blank=True)
+	venues = models.ForeignKey(Venue, blank=True)
+
+
+class Venue(models.Model):
+	romanized_name = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
-	url = models.UrlField()
-	start_date = models.DateField()
-	end_date = models.DateField()
+	location = models.CharField(max_length=200)
+	country = models.CharField(max_length=200, blank=True)
+
--- base/goods/models.py
+++ base/goods/models.py
@@ -2,16 +2,17 @@ from django.db import models
 	
 
 class Source(models.Model):
+	romanized_name = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
-	url = models.UrlField(blank=True)
-	
-	
+	url = models.URLField(blank=True)
+
+
 class Good(models.Model):
+	romanized_name = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
 	event = models.ForeignKey(Event, blank=True)
 	source = models.ForeignKey(Source, blank=True)
 	available_from = models.DateField(blank=True)
 	available_until = models.DateField(blank=True)
-	url = models.UrlFeild(blank=True)
+	url = models.URLFeild(blank=True)
 	image = models.ImageField(blank=True)
-	
