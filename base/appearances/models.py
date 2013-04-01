af7f91c61dfda4a4b65d4307af19e8173c049b76
Jen Veloso <jen@revyver.com>
2013-03-31 21:00:42 -0700

    Added TimeSlot, EpisodeSynopsis and IssueImage models and adjusted accordingly
    Such as moving the attributes that made more sense for the new model.

--- base/appearances/models.py
+++ base/appearances/models.py
@@ -6,13 +6,13 @@ class Show(models.Model):
 	description = models.TextField(blank=True)
 	aired_from = models.DateField(blank=True, null=True)
 	aired_until = models.DateField(blank=True, null=True)
+	# Aired from/until is only for HelloPro shows.
 	
 class TimeSlot(models.Model):
 	start_time = models.DateTimeField(blank=True, null=True)
 	end_time = models.DateTimeField(blank=True, null=True)
 	show = models.ForeignKey(Show)
-	# Aired from/until only for HelloPro shows.
-	# Start/end time only for HelloPro shows (to derrive in Episodes model).
+	# TimeSlot is only for HelloPro shows.
 
 
 class Episode(models.Model):
@@ -24,15 +24,21 @@ class Episode(models.Model):
 	synopsis = models.TextField(blank=True)
 	video_link = models.URLField(blank=True)
 	show = models.ForeignKey(Show)
-	# Look into smart way to derrive episode number for HelloPro shows.
-	# Look into linking episode if they're continuations.
-	# Multiple synopsese will be submitted by users.
+	episode = models.ForeignKey('self', blank=True, null=True, related_name='continuation')
+	# Continuation links episodes that are continuations of each other (i.e. Part One, Part Two).
 	# Embed video if possible. Multiple links will be submitted by users.
+
+
+class EpisodeSynopsis(models.Model):
+	body = models.TextField(blank=True)
+	# Multiple synopsese will be submitted by users.
 
 
 class Magazine(models.Model):
 	romanized_name = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
+	price = models.IntegerField(blank=True, null=True)
+	# If prices change in the course of a magazine we may need to move this to Issue or make a Price model.
 
 
 class Issue(models.Model):
@@ -41,17 +47,20 @@ class Issue(models.Model):
 	release_date = models.DateField(blank=True, null=True)
 	catalog_number = models.CharField(blank=True)
 	cover = models.ImageField(blank=True)
-	price = models.IntegerField(blank=True, null=True)
 	isbn_number = models.CharField(max_length=19) # ?
-	gallery = models.ImageField(blank=True)
-	# Possibly move price to Magazine model.
-	# Gallery will allow multiple images to be uploaded by users.
-
+	
 	def all_available_cards(self):
 		return self.available_cards.all()
 
 
+class IssueImage(models.Model):
+	image = models.ImageField(blank=True)
+	issue = models.ForeignKey(Issue, related_name='gallery')
+	# Gallery will allow multiple images to be uploaded by users.
+
+
 class Set(models.Model):
+	romanized_name = models.CharField(max_length=200)
 	issue = models.ForeignKey(Issue, related_name='sets')
 	image = models.ImageField(blank=True)
 
@@ -59,7 +68,7 @@ class Set(models.Model):
 class Card(models.Model):
 	issue = models.ForeignKey(Issue, related_name='cards')
 	set = models.ForeignKey(Set, blank=True, null=True)
-	model = models.ForeignKey(Idol, blank=True, related_name='cards')
+	model = models.ForeignKey(Idol, related_name='cards')
 	image = models.ImageField(blank=True)
 	# Sometimes multiple models will be featured in a card.
 	# Derrive H!B idol from existing database, but group they belong to set manually.
