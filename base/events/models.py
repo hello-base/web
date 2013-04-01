58c0463c5573dc46356308fdac5f47482b10815a
Jen Veloso <jen@revyver.com>
2013-03-31 21:01:42 -0700

    Added null=True where it was missing and added related name 'schedule' to event ForeignKey
    Check with Bryan whether that was the right way to use related_name.

--- base/events/models.py
+++ base/events/models.py
@@ -4,15 +4,15 @@ class Event(models.Model):
 	romanized_name = models.CharField(max_length=200)
 	name = models.CharField(max_lenth=200)
 	info_link = models.URLField(blank=True)
-	start_date = models.DateField(blank=True)
-	end_date = models.DateField(blank=True)
+	start_date = models.DateField(blank=True, null=True)
+	end_date = models.DateField(blank=True, null=True)
 
 
 class Performance(models.Model):
 	day = models.DateField()
-	start_time = models.TimeField(blank=True)
-	end_time = models.TimeField(blank=True)
-	event = models.ForeignKey(Event)
+	start_time = models.TimeField(blank=True, null=True)
+	end_time = models.TimeField(blank=True, null=True)
+	event = models.ForeignKey(Event, related_name='schedule')
 
 
 class Venue(models.Model):
