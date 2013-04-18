36d59ca8f9444840540cac2445b5fea17b5cb9f3
Jen Veloso <jen@revyver.com>
2013-04-18 16:26:25 -0700

    added GoodSet with CATEGORIES and refined existing CATEGORIES
    Changed name from TYPES to CATEGORIES.

--- base/goods/models.py
+++ base/goods/models.py
@@ -18,59 +18,52 @@ class Source(models.Model):
 
 
 class Good(models.Model):
-	TYPES = Choices(
-		('album', 'Album'), # other?
+	CATEGORIES = Choices(
 		('badge', 'Badge'),
 		('bandana', 'Bandana'), # other?
-		('bandanaset', 'Bandana Set'), # with photos. other?
 		('clearfile', 'Clear File'),
-		('clearfileset', 'Clear File Set'), # with photos
 		('collectionphoto', 'Collection Photo'),
-		('collectionphotoset', 'Collection Photo Set'), # set?
 		('costume', 'Costume Good'), # other? costume fabric piece
 		('dvd', 'DVD'), # DVD Magazine/DVD Memorial.. NO store-released DVD's.
 		('gachagacha', 'GachaGacha Collection Item'),
-		('gachagachaset', 'GachaGacha Collection Set'),
 		('keyholder', 'Keyholder'),
-		('keyholderset', 'Keyholder Set'), # with photos
 		('microfiber', 'Microfiber Towel'),
 		('muffler', 'Muffler Towel'),
 		('parka', 'Parka'), # hoodie?
-		('parkaset', 'Parka Set'), # with photos. hoodie?
 		('tradingcardphoto', 'Trading Card-size Photo'),
 		('lphoto', 'L-size Photo'),
+		('lmetallic', 'L-size Metallic Photo'),
 		('postcardphoto', 'Postcard-size Photo'),
 		('2lphoto', '2L-size Photo'),
 		('A5photo', 'A5-wide Photo'),
+		('A4photo', 'A4-size Photo'),
 		('photocard', 'Photo Card'), # PR cards, 4 seasons cards, birthday cards, etc.
 		('pinup', 'Pin-up Poster'),
-		('pinupset', 'Pin-up Poster Set'), # set?
 		('a2poster', 'A2-size Poster'),
 		('b2poster', 'B2-size Poster'),
 		('scrunchie', 'Scrunchie'), # other? shushu?
-		('scrunchieset', 'Scrunchie Set'), # with photos. other? shushu set?
 		('strap', 'Strap/Charm'),
-		('strap', 'Strap/Charm'), # with photos
 		('tourbag', 'Tour Bag'),
 		('tradingcard', 'Trading Card'), # non-collection?
 		('tshirt', 'T-shirt'),
-		('tshirtset', 'T-shirt Set'), # with photos/wristband
 		('uchiwa', 'Uchiwa'),
 		('visualbook', 'Visual Book/Pamphlet'),
 		('visualscreen', 'Visual Screen'), # other? tapestry?
 		('wristband', 'Wristband'),
 		('collectionitem', 'Other Collection Item'),
-		('collectionset', 'Other Collection Set'),
 		('poster', 'Other Poster'),
 		('towel', 'Other Towel'),
 		('other', 'Other'),
 	)
-	type = models.CharField(choices=TYPES)
+	category = models.CharField(choices=CATEGORIES)
 	is_graduation_good = models.BooleanField(default=False)
+	is_birthday_good = models.BooleanField(default=False)
 	is_campaign_good = models.BooleanField(default=False)
 	is_lottery_good = models.BooleanField(default=False)
 	is_bonus_good = models.BooleanField(default=False)
 	is_set_exclusive = models.BooleanField(default=False)
+	is_online_exclusive = models.BooleanField(default=False)
+	is_mailorder_exclusive = models.BooleanField(default=False)
 	romanized_name = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
 	price = models.IntegerField(blank=True, null=True)
@@ -80,8 +73,27 @@ class Good(models.Model):
 	available_until = models.DateField(blank=True, null=True)
 	link = models.URLFeild(blank=True)
 	image = models.ImageField(blank=True)
-	parent = models.ForeignKey(blank=True, null=True, 'self')
+	parent = models.ForeignKey('GoodSet', blank=True, null=True)
 	# Look into goods that are part of an event having the same available from/until date.
 	# A Good is either from a Source or from an Event, not both. Exception: HelloShop.jp Goods section???
 	# Connect to idols/groups (custom ForeignKey?)
-	# Instead of 'Set', make individual items in a set have a parent?
+	# Goods that you get with releases. Example: Mano NFS photo for buying any of her PB's during graduation.
+	
+	
+class GoodSet(Good):
+	CATEGORIES = Choices(
+		('bandanaset', 'Bandana Set'),
+		('clearfileset', 'Clear File Set'),
+		('collectionphotoset', 'Collection Photo Set'),
+		('gachagachaset', 'GachaGacha Collection Set'),
+		('keyholderset', 'Keyholder Set'),
+		('parkaset', 'Parka Set'), # hoodie set?
+		('pinupset', 'Pin-up Poster Set'),
+		('scrunchieset', 'Scrunchie Set'), # other? shushu set?
+		('strapset', 'Strap/Charm Set'),
+		('tshirtset', 'T-shirt Set'),
+		('collectionset', 'Other Collection Set'),
+		('otherset', 'Other Set'),
+	)
+	category = models.CharField(choices=CATEGORIES)
+	# Sets within sets. Setception.
