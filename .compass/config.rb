182ddd71c75fede4def658ae3d02c01adc302605
Bryan Veloso <bryan@revyver.com>
2013-04-21 15:59:37 -0700

    Prefer Sass syntax this time?

--- .compass/config.rb
+++ .compass/config.rb
@@ -25,6 +25,6 @@ line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass ranking/static/scss scss && rm -rf sass && mv scss sass
