afa285bfdd9b16eacbdd68342b35a3d72d23db6d
Bryan Veloso <bryan@revyver.com>
2013-04-21 10:19:24 -0700

    Basic production settings.

---
+++ base/settings/production.py
@@ -0,0 +1,51 @@
+import os
+
+from common import Base as Settings
+
+
+class Production(Settings):
+    DEBUG = True
+
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
+
+    # Middleware
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.gzip.GZipMiddleware',
+        'djangosecure.middleware.SecurityMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'social_auth.middleware.SocialAuthExceptionMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'django_hosts.middleware.HostsMiddleware',
+    )
+
+    # Installed Applications
+    INSTALLED_APPS = Settings.INSTALLED_APPS + [
+        'djangosecure',
+    ]
+
+    # Django Secure
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_FRAME_DENY = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_SECONDS = 1800
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+
+    # Secret Key
+    SECRET_KEY = os.environ.get('SECUREKEY_VIOLET_KEY', '').split(',')[0]
+
+    # Heroku =================================================================
+    # Sentry
+    if 'SENTRY_DSN' in os.environ:
+        # Add raven to the list of installed apps
+        INSTALLED_APPS = Settings.INSTALLED_APPS + ['raven.contrib.django', ]
