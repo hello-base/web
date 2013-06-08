f48bcd85b50f555a2591c3009faec6f9eaaddb75
Bryan Veloso <bryan@revyver.com>
2013-06-08 13:59:24 -0700

    Add my utils module from H!R.

---
+++ base/components/people/utils.py
@@ -0,0 +1,29 @@
+from datetime import date, timedelta
+
+from django.db.models import get_model
+
+
+def calculate_age(birthdate):
+    today = date.today()
+    if birthdate is not None:
+        birthday = birthdate.replace(year=today.year)
+        if birthday > today:
+            return today.year - birthdate.year - 1
+        else:
+            return today.year - birthdate.year
+
+
+def calculate_average_age(birthdates):
+    age_timedelta = sum((date.today() - birthdate for birthdate in birthdates), timedelta(0)) / len(birthdates)
+    average_age = round(age_timedelta.days / (365.2425), 2)
+    return average_age
+
+
+def attach_primary_groups(idols):
+    roster = list(idols)
+    primary_groups = get_model('people', 'membership').objects.get_primary_groups(roster)
+
+    for idol in roster:
+        idol.primary_group, idol.is_primary_group_leader = primary_groups.get(idol.id)
+
+    return roster
