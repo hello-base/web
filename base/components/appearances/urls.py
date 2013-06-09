1cd803d00b835a27e342124dd2885e7b819e8bc3
Jen Veloso <jen@revyver.com>
2013-06-08 23:13:05 -0700

    First attempt at forms!
    This is bound to crash and burn.

--- base/components/appearances/urls.py
+++ base/components/appearances/urls.py
@@ -4,6 +4,7 @@ from .views import MagazineListView, MagazineDetailView, IssueDetailView, ShowLi
 
 
 urlpatterns = patterns('',
+
     url(r'^show/(?P<slug>[-\w]+)/$', name='show-detail', view=ShowDetailView.as_view()),
     url(r'^show/$', name='show-list', view=ShowListView.as_view()),
     
@@ -12,5 +13,15 @@ urlpatterns = patterns('',
     url(r'^magazine/(?P<slug>[-\w]+)/$', name='magazine-detail', view=MagazineDetailView.as_view()),
     url(r'^magazine/$', name='magazine-list', view=MagazineListView.as_view()),
     
-    url(r'^appearances/issue/(?P<volume_number>[\d\+]+)/$', name='issue-detail', view=IssueDetailView.as_view()),
+    url(r'^magazine/issue/(?P<volume_number>[\d\+]+)/$', name='issue-detail', view=IssueDetailView.as_view()),
+    
+    url(r'^show/episode/create/$', name='episode-create', view=ShowCreateView.as_view()),
+    url(r'^show/create/$', name='show-create', view=ShowCreateView.as_view()),
+    url(r'^magazine/issue/create/$', name='issue-create', view=ShowCreateView.as_view()),
+    url(r'^magazine/create/$', name='magazine-create', view=ShowCreateView.as_view()),
+    
+    url(r'^show/episode/update/(?P<pk>\d+)/$', name='episode-update', view=ShowUpdateView.as_view()),
+    url(r'^show/update/(?P<pk>\d+)/$', name='show-update', view=ShowUpdateView.as_view()),
+    url(r'^magazine/issue/update/(?P<pk>\d+)/$', name='issue-update', view=ShowUpdateView.as_view()),
+    url(r'^magazine/update/(?P<pk>\d+)/$', name='magazine-update', view=ShowUpdateView.as_view()),
 )
--- base/components/appearances/views.py
+++ base/components/appearances/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView, TemplateView
+from django.core.urlresolvers import reverse_lazy
 
 from .models import Show, Episode, Magazine, Issue
 
@@ -13,9 +14,33 @@ class ShowDetailView(DetailView):
     template_name = 'appearances/show-detail.html'
 
 
+class ShowCreateView(CreateView):
+    model = Show
+    success_url = reverse_lazy('show-detail')
+    form_class = ShowForm
+
+
+class ShowUpdateView(UpdateView):
+    model = Show
+    success_url = reverse_lazy('show-detail')
+    form_class = ShowForm
+
+
 class EpisodeDetailView(DetailView):
     queryset = Episode.objects.all()
     template_name = 'appearances/episode-detail.html'
+
+
+class EpisodeCreateView(CreateView):
+    model = Episode
+    success_url = reverse_lazy('episode-detail')
+    form_class = EpisodeForm
+
+
+class EpisodeUpdateView(UpdateView):
+    model = Episode
+    success_url = reverse_lazy('episode-detail')
+    form_class = EpisodeForm
 
 
 class MagazineListView(ListView):
@@ -28,6 +53,30 @@ class MagazineDetailView(DetailView):
     template_name = 'appearances/magazine-detail.html'
 
 
+class MagazineCreateView(CreateView):
+    model = Magazine
+    success_url = reverse_lazy('magazine-detail')
+    form_class = MagazineForm
+
+
+class MagazineUpdateView(UpdateView):
+    model = Magazine
+    success_url = reverse_lazy('magazine-detail')
+    form_class = MagazineForm
+
+
 class IssueDetailView(DetailView):
     queryset = Issue.objects.all()
     template_name = 'appearances/issue-detail.html'
+
+
+class IssueCreateView(CreateView):
+    model = Issue
+    success_url = reverse_lazy('issue-detail')
+    form_class = IssueForm
+
+
+class IssueUpdateView(UpdateView):
+    model = Issue
+    success_url = reverse_lazy('issue-detail')
+    form_class = IssueForm
