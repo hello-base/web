e300f7d3038cc6f07e17bb6ad8049192925583a0
Bryan Veloso <bryan@github.com>
2013-07-13 12:53:18 -0700

    Fleshing out. Every piece of merchandise is associated with a group or idol.

--- base/components/merchandise/models.py
+++ base/components/merchandise/models.py
@@ -1,10 +1,18 @@
+from datetime import date
+
 from ohashi.db import models
 
 
 class Merchandise(models.Model):
+    idols = models.ManyToManyField('people.Idol', blank=True, null=True, related_name='%(class)ss')
+    groups = models.ManyToManyField('people.Group', blank=True, null=True, related_name='%(class)ss')
+
     romanized_name = models.CharField()
     name = models.CharField(blank=True)
-    released = models.DateField(blank=True)
+    released = models.DateField(blank=True, db_index=True, default=date.min, null=True)
+
+    # Secondary Identifier
+    uuid = models.UUIDField()
 
     class Meta:
         abstract = True
