7cdfd3b369a2d55fcaea2b4d303ba55d6976aaa8
Jen Veloso <jen@revyver.com>
2013-07-14 17:01:12 -0700

    Added/edited fields as needed after testing with actual data
    Needed secondary_info_link for Event and name/romanized_name to
    Performance (thanks Rival Survival), former_names for Venue (thanks JCB
    Hall), and foreign key to Venue from Performance.

--- base/components/events/models.py
+++ base/components/events/models.py
@@ -7,8 +7,9 @@ from model_utils import FieldTracker
 class Event(models.Model):
     romanized_name = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    nickname = models.CharField(blank=True, max_length=30)
-    info_link = models.URLField(blank=True)
+    nickname = models.CharField(max_length=30, blank=True, null=True)
+    info_link = models.URLField(blank=True, null=True)
+    secondary_info_link = models.URLField(blank=True, null=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
     slug = models.SlugField()
@@ -20,30 +21,33 @@ class Event(models.Model):
         return u'%s' % self.romanized_name
 
 
+class Venue(models.Model):
+    romanized_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    former_names = models.CharField(max_length=200, blank=True, null=True)
+    romanized_address = models.CharField(max_length=200, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    country = models.CharField(max_length=200, blank=True, null=True)
+    slug = models.SlugField()
+    # Country field only filled if outside US (maybe unnecessary).
+
+    def __unicode__(self):
+        return u'%s' % self.romanized_name
+
 class Performance(models.Model):
     day = models.DateField()
+    romanized_name = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
     start_time = models.TimeField(blank=True, null=True)
     end_time = models.TimeField(blank=True, null=True)
     event = models.ForeignKey(Event, related_name='schedule')
+    venue = models.ForeignKey(Venue, blank=True, null=True, related_name='performances')
     # Add 'set list' field with convoluted ordering and everything...
 
     def __unicode__(self):
         if self.start_time:
             return u'%s %s at %s' % (self.day, self.event.nickname, self.start_time)
         return u'%s %s' % (self.day, self.event.nickname)
-
-
-class Venue(models.Model):
-    romanized_name = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200) # is CharField good enough to search by location eventually??
-    country = models.CharField(max_length=200, blank=True)
-    performance = models.ForeignKey(Performance)
-    slug = models.SlugField()
-    # Country field only filled if outside US (maybe unnecessary).
-
-    def __unicode__(self):
-        return u'%s' % self.romanized_day
 
 
 # -HACK- to quickly input data.
