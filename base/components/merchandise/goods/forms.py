18854205c44be491c9d042632c94da367950e4d2
Bryan Veloso <bryan@github.com>
2013-07-24 12:03:54 -0700

    Add a BaseGoodForm to share its clean() method across subclasses.

--- base/components/merchandise/goods/forms.py
+++ base/components/merchandise/goods/forms.py
@@ -19,25 +19,17 @@ class ShopForm(forms.ModelForm):
         model = models.Shop
 
 
-class GoodForm(forms.ModelForm):
-    class Meta:
-        model = models.Good
-        fields = BASE_GOOD_FIELDS + [
-            'category', 'parent', 'is_bonus_good', 'is_campaign_good',
-            'is_lottery_good', 'is_set_exclusive'
-        ]
-        widgets = {
-            'available_from': forms.DateInput,
-            'available_until': forms.DateInput,
-            'link': forms.URLInput,
-            'name': forms.TextInput,
-            'other_info': forms.Textarea(attrs={'rows': 2}),
-            'price': forms.NumberInput,
-            'romanized_name': forms.TextInput,
-        }
-
+class BaseGoodForm(forms.ModelForm):
     def clean(self):
-        cleaned_data = super(GoodForm, self).clean()
+        cleaned_data = super(BaseGoodForm, self).clean()
+
+        # Of course the date a good is available until cannot be earlier than
+        # the date it was released.
+        available_from = cleaned_data.get('available_from', '')
+        available_until = cleaned_data.get('available_until', '')
+        if (available_from and available_until) and (available_until < available_from):
+            message = u'The "Available Until" date is earlier than "Available From".'
+            raise forms.ValidationError(message)
 
         # Goods must be associated with at least one idol or one group. Raise
         # a ValidationError if none are associated.
@@ -58,11 +50,29 @@ class GoodForm(forms.ModelForm):
         return cleaned_data
 
 
-class SetForm(forms.ModelForm):
+class GoodForm(BaseGoodForm):
+    class Meta:
+        model = models.Good
+        fields = BASE_GOOD_FIELDS + [
+            'category', 'parent', 'is_bonus_good', 'is_campaign_good',
+            'is_lottery_good', 'is_set_exclusive'
+        ]
+        widgets = {
+            'available_from': forms.DateInput,
+            'available_until': forms.DateInput,
+            'link': forms.URLInput,
+            'name': forms.TextInput,
+            'other_info': forms.Textarea(attrs={'rows': 2}),
+            'price': forms.NumberInput,
+            'romanized_name': forms.TextInput,
+        }
+
+
+class SetForm(BaseGoodForm):
     class Meta:
         model = models.Set
 
 
-class SuperSetForm(forms.ModelForm):
+class SuperSetForm(BaseGoodForm):
     class Meta:
         model = models.SuperSet
