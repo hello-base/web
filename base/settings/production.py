c40d2e8f987b7a3d5e67c53e7717125210a0084e
Bryan Veloso <bryan@github.com>
2013-08-07 14:49:33 -0700

    Comment all of the damn things (also for debugging).

--- base/settings/production.py
+++ base/settings/production.py
@@ -6,64 +6,64 @@ from .base import Base as Settings
 class Production(Settings):
     DEBUG = True
 
-    TEMPLATE_LOADERS = (
-        ('django.template.loaders.cached.Loader', (
-            'django.template.loaders.filesystem.Loader',
-            'django.template.loaders.app_directories.Loader',
-        )),
-    )
+    # TEMPLATE_LOADERS = (
+    #     ('django.template.loaders.cached.Loader', (
+    #         'django.template.loaders.filesystem.Loader',
+    #         'django.template.loaders.app_directories.Loader',
+    #     )),
+    # )
 
-    # Middleware
-    MIDDLEWARE_CLASSES = (
-        'django.middleware.gzip.GZipMiddleware',
-        'djangosecure.middleware.SecurityMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'social_auth.middleware.SocialAuthExceptionMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'django_hosts.middleware.HostsMiddleware',
-    )
+    # # Middleware
+    # MIDDLEWARE_CLASSES = (
+    #     'django.middleware.gzip.GZipMiddleware',
+    #     'djangosecure.middleware.SecurityMiddleware',
+    #     'django.middleware.common.CommonMiddleware',
+    #     'django.contrib.sessions.middleware.SessionMiddleware',
+    #     'django.middleware.csrf.CsrfViewMiddleware',
+    #     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #     'django.contrib.messages.middleware.MessageMiddleware',
+    #     'social_auth.middleware.SocialAuthExceptionMiddleware',
+    #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #     'django_hosts.middleware.HostsMiddleware',
+    # )
 
-    # Installed Applications
-    INSTALLED_APPS = Settings.INSTALLED_APPS + [
-        'djangosecure',
-    ]
+    # # Installed Applications
+    # INSTALLED_APPS = Settings.INSTALLED_APPS + [
+    #     'djangosecure',
+    # ]
 
-    # Logging
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-            }
-        },
-        'handlers': {
-            'console': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
-            },
-        },
-        'loggers': {
-            'django': {
-                'handlers': ['console'],
-                'level': 'DEBUG',
-                'propagate': True,
-            },
-        }
-    }
+    # # Logging
+    # LOGGING = {
+    #     'version': 1,
+    #     'disable_existing_loggers': False,
+    #     'filters': {
+    #         'require_debug_false': {
+    #             '()': 'django.utils.log.RequireDebugFalse'
+    #         }
+    #     },
+    #     'handlers': {
+    #         'console': {
+    #             'level': 'DEBUG',
+    #             'class': 'logging.StreamHandler',
+    #         },
+    #     },
+    #     'loggers': {
+    #         'django': {
+    #             'handlers': ['console'],
+    #             'level': 'DEBUG',
+    #             'propagate': True,
+    #         },
+    #     }
+    # }
 
-    # Django Secure
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_FRAME_DENY = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 1800
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
+    # # Django Secure
+    # SECURE_BROWSER_XSS_FILTER = True
+    # SECURE_CONTENT_TYPE_NOSNIFF = True
+    # SECURE_FRAME_DENY = True
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    # SECURE_HSTS_SECONDS = 1800
+    # SECURE_SSL_REDIRECT = True
+    # SESSION_COOKIE_SECURE = True
 
     # Secret Key
     SECRET_KEY = os.environ.get('SECUREKEY_VIOLET_KEY', '').split(',')[0]
