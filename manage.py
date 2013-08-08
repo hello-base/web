b37a4d3b6709da95f2a2a876665305424587c02b
Bryan Veloso <bryan@github.com>
2013-08-07 17:40:03 -0700

    Pin and use django-dotenv.

--- manage.py
+++ manage.py
@@ -1,34 +1,14 @@
 #!/usr/bin/env python
 import os
-import re
 import sys
-
-
-def read_env():
-    try:
-        with open('.env') as f:
-            content = f.read()
-    except IOError:
-        content = ''
-
-    for line in content.splitlines():
-        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
-        if m1:
-            key, val = m1.group(1), m1.group(2)
-            m2 = re.match(r"\A'(.*)'\Z", val)
-            if m2:
-                val = m2.group(1)
-            m3 = re.match(r'\A"(.*)"\Z', val)
-            if m3:
-                val = re.sub(r'\\(.)', r'\1', m3.group(1))
-            os.environ.setdefault(key, val)
 
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
     os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
 
-    read_env()
+    import dotenv
+    dotenv.read_dotenv('.env')
 
     from configurations.management import execute_from_command_line
     execute_from_command_line(sys.argv)
