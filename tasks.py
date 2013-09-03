c434d182d2f6e0535314e8fcdbac01324bf9395b
Bryan Veloso <bryan@revyver.com>
2013-09-02 17:30:06 -0700

    Add our little deploy script to H!B.

---
+++ tasks.py
@@ -0,0 +1,20 @@
+from invoke import run, task
+
+@task
+def deploy():
+    print('So you want to deploy? Let\'s get started.')
+
+    # Static Files.
+    print('- Run the stylesheets through Compass using "Production" settings...')
+    run('compass compile -e production --force -q')
+
+    print('- Collecting the static files and throwing them on S3...')
+    run('python manage.py collectstatic --configuration=Production --noinput -v 0')
+
+    # Heroku.
+    print('- Deploying Hello! Base to Heroku...')
+    run('git push heroku master')
+
+    # Done!
+    print('')
+    print('All done!')
