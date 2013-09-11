8318bae21bd5cb716a4cbf2cd2dfe46ea8cadbcf
Bryan Veloso <bryan@revyver.com>
2013-09-11 01:00:27 -0700

    Hide .env behind a development environment.

--- manage.py
+++ manage.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
     os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
 
-    import dotenv
-    dotenv.read_dotenv('.env')
+    if os.environ['DJANGO_CONFIGURATION'] == 'Development':
+        import dotenv
+        dotenv.read_dotenv('.env')
 
     from configurations.management import execute_from_command_line
     execute_from_command_line(sys.argv)
