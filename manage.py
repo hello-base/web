840404924e901bf86eb6256419b9af03ee14400d
Bryan Veloso <bryan@revyver.com>
2013-09-11 01:09:46 -0700

    Fallin' on my ass here. :<

--- manage.py
+++ manage.py
@@ -7,9 +7,8 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
     os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
 
-    if os.environ['DJANGO_CONFIGURATION'] == 'Development':
-        import dotenv
-        dotenv.read_dotenv('.env')
+    import dotenv
+    dotenv.read_dotenv()
 
     from configurations.management import execute_from_command_line
     execute_from_command_line(sys.argv)
