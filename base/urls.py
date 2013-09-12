0fd1eb429553cfe9c65b86d2ebbd5c1aeaca801d
Bryan Veloso <bryan@revyver.com>
2013-09-11 20:05:17 -0700

    Implement facets. Not gonna use them for drilling though.

--- base/urls.py
+++ base/urls.py
@@ -1,14 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from haystack.forms import FacetedSearchForm
+from haystack.query import SearchQuerySet
+from haystack.views import FacetedSearchView
+
 from components.views import AutocompleteView, SiteView
 
 # Uncomment the next two lines to enable the admin:
 # admin.autodiscover()
 
+# The switch to faceted search requires a custom SearchQuerySet.
+# By default we facet on "model" so the types of results can be
+# displayed on the search results page.
+sqs = SearchQuerySet().facet('model')
+
 urlpatterns = patterns('',
     url(r'^$', name='site-home', view=SiteView.as_view()),
-    url(r'^search/', include('haystack.urls')),
+    url(r'^search/', name='search', view=FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs)),
     url(r'^search/autocomplete/$', view=AutocompleteView.as_view()),
 
     url(r'^', include('components.appearances.urls')),
