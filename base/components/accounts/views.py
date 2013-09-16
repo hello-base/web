0ff7185c89dc58f1e9093a658dab84da74105095
Bryan Veloso <bryan@revyver.com>
2013-09-16 09:39:58 -0700

    We were missing redirect_uri. :(

--- base/components/accounts/views.py
+++ base/components/accounts/views.py
@@ -9,12 +9,13 @@ CLIENT_ID = settings.HELLO_BASE_CLIENT_ID
 CLIENT_SECRET = settings.HELLO_BASE_CLIENT_SECRET
 AUTHORIZATION_URL = 'https://localhost:8443/authorize/'
 TOKEN_URL = 'https://localhost:8443/token/'
+REDIRECT_URL = 'https://localhost:8444/accounts/authenticated/'
 
 class PreAuthorizationView(RedirectView):
     def get(self, request, *args, **kwargs):
         # Redirect the user to Hello! Base ID using the appropriate
         # URL with a few key OAuth paramters built in.
-        base = OAuth2Session(CLIENT_ID)
+        base = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URL)
         authorization_url, state = base.authorization_url(AUTHORIZATION_URL)
 
         # State is used to prevent CSRF, so let's keep this for later.
@@ -28,11 +29,7 @@ class PostAuthorizationView(View):
         # We SHOULD be grabbing state from the session, maybe this'll
         # work in production, but in development it's not. So... we're
         # doing this. :(
-        base = OAuth2Session(CLIENT_ID, state=request.GET['state'])
-        token = base.fetch_token(
-            TOKEN_URL,
-            auth=(CLIENT_ID, CLIENT_SECRET),
-            authorization_response=request.build_absolute_uri()
-        )
-
+        base = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URL, state=request.GET['state'])
+        token = base.fetch_token(TOKEN_URL, auth=(CLIENT_ID, CLIENT_SECRET), authorization_response=request.build_absolute_uri())
+        return http.HttpResponse(token)
 
