4f6d8751cac5b8abac7af3a9b2db04d69ad07208
Bryan Veloso <bryan@revyver.com>
2013-09-16 18:39:34 -0700

    A VERY naive authentication backend. But... this is a closed system.

--- base/components/accounts/backends.py
+++ base/components/accounts/backends.py
@@ -1,22 +1,19 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
 
 from oauthlib.oauth2.rfc6749.endpoints import resource
 
 
 USER = get_user_model()
 
-class OAuth2Backend(object):
-    """
-    Authenticate against an OAuth2 access token.
-
-    """
-    def authenticate(self, **credentials):
-        request = credentials.get('request')
-        if request is not None:
-            is_valid, request = resource.verify_request(request, scopes=[])
-            if is_valid:
-                return request.user
-        return None
+class HelloBaseIDBackend(ModelBackend):
+    def authenticate(self, username=None):
+        try:
+            user = USER.objects.filter(username=username)[0]
+        except IndexError:
+            return None
+        else:
+            return user
 
     def get_user(self, user_id):
         try:
