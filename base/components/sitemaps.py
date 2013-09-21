6a92c62ab4983b4caa6e969a208dc4fa011e7e40
Bryan Veloso <bryan@revyver.com>
2013-09-21 11:22:00 -0700

    Really add me some sitemaps.

---
+++ base/components/sitemaps.py
@@ -0,0 +1,57 @@
+from django.contrib.sitemaps import Sitemap
+
+from merchandise.music.models import Album, Single
+from people.constants import STATUS
+from people.models import Group, Idol
+
+
+class AlbumSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.6
+
+    def items(self):
+        return Album.objects.all()
+
+    def lastmod(self, obj):
+        return obj.modified
+
+
+class IdolSitemap(Sitemap):
+    changefreq = 'monthly'
+
+    def items(self):
+        return Idol.objects.all()
+
+    def lastmod(self, obj):
+        return obj.modified
+
+    def priority(self, obj):
+        if obj.status == STATUS.active:
+            return 0.75
+        return 0.5
+
+
+class GroupSitemap(Sitemap):
+    changefreq = 'monthly'
+
+    def items(self):
+        return Group.objects.all()
+
+    def lastmod(self, obj):
+        return obj.modified
+
+    def priority(self, obj):
+        if obj.status == STATUS.active:
+            return 0.75
+        return 0.5
+
+
+class SingleSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.6
+
+    def items(self):
+        return Single.objects.all()
+
+    def lastmod(self, obj):
+        return obj.modified
