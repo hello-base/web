f8c94e346d4d6a0dedb62451e929ed6bcbd2ee1d
Bryan Veloso <bryan@revyver.com>
2013-09-30 18:40:10 -0700

    Rewrite tasks.py, use this as our main invoker... (lol).

--- tasks.py
+++ tasks.py
@@ -1,37 +1,60 @@
-from invoke import run, task
+import functools
 
-@task
-def deploy():
+import invoke
+
+
+def _out(name, message):
+    print("[\033[1;37m{}\033[0m] {}".format(name, message))
+
+
+@invoke.task(name='deploy')
+def deploy(**kwargs):
     print('So you want to deploy? Let\'s get started.')
+
+    # Compile the Handlebars.
+    print('- Compile all of the Handlebars...')
+    invoke.run('handlebars base/templates/partials/handlebars -f static/javascripts/templates.js')
 
     # Static Files.
     print('- Run the stylesheets through Compass using "Production" settings...')
-    run('compass compile -e production --force -q')
+    invoke.run('compass compile -e production --force -q')
 
     print('- Collecting the static files and throwing them on S3...')
-    run('python manage.py collectstatic --configuration=Production --noinput -v 0')
+    invoke.run('python manage.py collectstatic --configuration=Production --noinput -v 0')
 
     # Heroku.
     print('- Deploying Hello! Base to Heroku...')
-    run('git push heroku master')
+    invoke.run('git push heroku master')
 
     # Done!
     print('')
     print('All done!')
 
 
-@task
-def compile():
-    # Compile the CSS.
-    print('- Run the stylesheets through Compass using "Production" settings...')
-    run('compass compile -e production --force -q')
-
-    # Compile the Handlebars.
-    print('- Compile all of the Handlebars...')
-    run('handlebars base/templates/partials/handlebars -f static/javascripts/templates.js')
-
-
-@task
-def server():
+@invoke.task(name='server')
+def development_server(**kwargs):
     # Use Foreman to start all the development processes.
-    run('foreman start -f Procfile.dev', pty=True)
+    out = functools.partial(_out, "development.server")
+    invoke.run('foreman start -f Procfile.dev', pty=True)
+
+
+@invoke.task(name='syncdb')
+def heroku_syncdb(**kwargs):
+    out = functools.partial(_out, "heroku.syncdb")
+    invoke.run('heroku run python manage.py syncdb')
+
+
+@invoke.task(name='collectstatic')
+def heroku_collectstatic(**kwargs):
+    out = functools.partial(_out, "heroku.collectstatic")
+    invoke.run('heroku run python manage.py collectstatic --noinput')
+
+
+ns = invoke.Collection(
+    deploy,
+    development_server,
+    heroku=invoke.Collection(
+        heroku_syncdb,
+        heroku_collectstatic
+    )
+)
