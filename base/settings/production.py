5fdce1d306bed8e61fc352262775ec6705b5c84c
Bryan Veloso <bryan@revyver.com>
2013-09-30 20:13:57 -0700

    Reformat the settings to match the format established by hello-base-id.

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 
@@ -8,19 +9,15 @@ from .base import Base as Settings
 
 
 class Production(Settings):
-    # Default / Debug Settings
-    DEBUG = False
-    ALLOWED_HOSTS = ['.hello-base.com', '.hello-base.com.',]
+    # Installed Applications (featuring Production).
+    # ------------------------------------------------------------------
+    INSTALLED_APPS = Settings.INSTALLED_APPS + [
+        'gunicorn',
+        'raven.contrib.django',
+    ]
 
-    # Template Settings
-    TEMPLATE_LOADERS = (
-        ('django.template.loaders.cached.Loader', (
-            'django.template.loaders.filesystem.Loader',
-            'django.template.loaders.app_directories.Loader',
-        )),
-    )
-
-    # Middleware
+    # Middleware Configuration.
+    # ------------------------------------------------------------------
     MIDDLEWARE_CLASSES = (
         'django.middleware.gzip.GZipMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -32,113 +29,141 @@ class Production(Settings):
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
 
-    # Installed Applications
-    INSTALLED_APPS = Settings.INSTALLED_APPS + [
-        'djangosecure',
-    ]
+    # Debug Settings.
+    # ------------------------------------------------------------------
+    DEBUG = values.BooleanValue(False)
 
+    # Secret Key Configuration.
+    # ------------------------------------------------------------------
+    SECRET_KEY = os.environ.get('SECUREKEY_AMBER_KEY', '').split(',')[0]
+
+    # Caching Configuration.
+    # ------------------------------------------------------------------
+    CACHES = redisify(default='redis://localhost')
+
+    # Separate the staticfiles cache from the normal Redis cache.
+    CACHES['staticfiles'] = {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'staticfiles',
+        # Long cache timeout for staticfiles, since this is used
+        # heavily by the optimizing storage.
+        'TIMEOUT': 60 * 60 * 24 * 365,
+    }
+
+    # django-secure.
+    # ------------------------------------------------------------------
+    INSTALLED_APPS += ['djangosecure',]
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    SECURE_FRAME_DENY = values.BooleanValue(True)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SESSION_COOKIE_SECURE = values.BooleanValue(True)
+    SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+
+    # Site Configuration.
+    # ------------------------------------------------------------------
+    ALLOWED_HOSTS = ['.hello-base.com', '.hello-base.com.',]
+
+    # Media Storage Configuration.
+    # ------------------------------------------------------------------
+    INSTALLED_APPS += [
+        's3_folder_storage',
+        'storages',
+    ]
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    STATICFILES_STORAGE = 'components.storage.S3PipelineStorage'
+
+    # Amazon Web Services
+    AWS_ACCESS_KEY_ID = values.SecretValue(environ_prefix='', environ_name='AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = values.SecretValue(environ_prefix='', environ_name='AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = values.SecretValue(environ_prefix='', environ_name='AWS_STORAGE_BUCKET_NAME')
+    AWS_AUTO_CREATE_BUCKET = True
+    AWS_PRELOAD_METADATA = True
+    AWS_QUERYSTRING_AUTH = False
+    AWS_IS_GZIPPED = False
+
+    # AWS Caching Settings
+    AWS_EXPIRY = 60 * 60 * 24 * 7
+    AWS_HEADERS = {'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY, AWS_EXPIRY)}
+
+    # django-s3-folder-storage
+    DEFAULT_S3_PATH = 'media'
+    STATIC_S3_PATH = 'static'
+    MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+    MEDIA_URL = '//%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, DEFAULT_S3_PATH)
+    STATIC_ROOT = '/%s/' % STATIC_S3_PATH
+    STATIC_URL = '//%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, STATIC_S3_PATH)
+
+    # Template Configuration.
+    # ------------------------------------------------------------------
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
+
+    # Authentication Configuration.
+    # ------------------------------------------------------------------
+    MEISHI_ENDPOINT = 'https://id.hello-base.com/api/'
+    OAUTH_AUTHORIZATION_URL = 'https://id.hello-base.com/oauth/authorize/'
+    OAUTH_TOKEN_URL = 'https://id.hello-base.com/oauth/token/'
+    OAUTH_REDIRECT_URL = 'https://hello-base.com/accounts/authenticated/'
+
+    # Logging Configuration.
+    # ------------------------------------------------------------------
     LOGGING = {
         'version': 1,
         'disable_existing_loggers': False,
-        'root': {
-            'level': 'WARNING',
-            'handlers': ['console', 'sentry'],
-        },
-        'formatters': {
-            'verbose': {
-                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-            },
-        },
         'filters': {
             'require_debug_false': {
                 '()': 'django.utils.log.RequireDebugFalse'
             }
         },
-        'handlers': {
-            'sentry': {
-                'level': 'ERROR',
-                'class': 'raven.contrib.django.handlers.SentryHandler',
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
+                'datefmt': '%d/%b/%Y %H:%M:%S'
             },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
+        },
+        'handlers': {
             'console': {
                 'level': 'DEBUG',
                 'class': 'logging.StreamHandler',
                 'formatter': 'verbose',
                 'stream': sys.stdout
             },
+            'sentry': {
+                'level': 'ERROR',
+                'class': 'raven.contrib.django.handlers.SentryHandler',
+                'filters': ['require_debug_false'],
+            },
         },
         'loggers': {
-            'analytics': {
-                'level': 'DEBUG',
-                'handlers': ['console', 'sentry'],
-                'propagate': True,
-            },
-            'django': {
-                'level': 'INFO',
-                'handlers': ['console', 'sentry'],
-                'propagate': True,
-            },
-            'django.db.backends': {
-                'level': 'ERROR',
-                'handlers': ['console', 'sentry'],
-                'propagate': False,
-            },
-            'django.request': {
-                'level': 'ERROR',
-                'handlers': ['console', 'sentry'],
-                'propagate': False,
-            },
-            'raven': {
+            # This is a "catch all" logger.
+            '': {
                 'level': 'DEBUG',
                 'handlers': ['console', 'sentry'],
                 'propagate': False,
             },
-            'sentry.errors': {
-                'level': 'DEBUG',
-                'handlers': ['console', 'sentry'],
-                'propagate': False,
-            },
-            'oauthlib': {
-                'level': 'DEBUG',
+            'boto': {
+                'level': 'WARNING',
                 'handlers': ['sentry'],
-                'propagate': True,
+                'propagate': False,
             },
-        },
+        }
     }
 
-    # File / Media / Static Media Settings
-    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
-    DEFAULT_S3_PATH = 'media'
-    MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
-    MEDIA_URL = '//%s.s3.amazonaws.com/media/' % Settings.AWS_STORAGE_BUCKET_NAME
-    STATIC_S3_PATH = 'static'
-    STATIC_ROOT = '/%s/' % STATIC_S3_PATH
-    STATIC_URL = '//%s.s3.amazonaws.com/static/' % Settings.AWS_STORAGE_BUCKET_NAME
-    STATICFILES_DIRS = (
-        os.path.normpath(os.path.join(Settings.SITE_ROOT, 'static')),
-    )
-    STATICFILES_STORAGE = 'components.storage.S3PipelineStorage'
 
-    # Django Authentication (OAuth, etc.)
-    MEISHI_ENDPOINT = 'https://id.hello-base.com/api/'
-    OAUTH_AUTHORIZATION_URL = 'https://id.hello-base.com/oauth/authorize/'
-    OAUTH_TOKEN_URL = 'https://id.hello-base.com/oauth/token/'
-    OAUTH_REDIRECT_URL = 'https://hello-base.com/accounts/authenticated/'
 
-    # Django Secure
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_FRAME_DENY = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-
-    # Secret Key
-    SECRET_KEY = os.environ.get('SECUREKEY_VIOLET_KEY', '').split(',')[0]
-
-    # Heroku ===========================================================
-    # Django Haystack (ElasticSearch)
+    # django-haystack (ElasticSearch).
+    # ------------------------------------------------------------------
     HAYSTACK_CONNECTIONS = {
         'default': {
             'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
@@ -147,24 +172,7 @@ class Production(Settings):
         },
     }
 
-    # OpenRedis
-    CACHES = redisify(default='redis://localhost')
-    SESSION_ENGINE = 'redis_sessions.session'
-    SESSION_REDIS_URL = os.environ.get('OPENREDIS_URL', '')
-
-    # Sentry
-    if 'SENTRY_DSN' in os.environ:
-        # Add raven to the list of installed apps
-        INSTALLED_APPS = Settings.INSTALLED_APPS + ['raven.contrib.django', ]
-
-    # Further StaticFiles Nonsense
-    # Separate the staticfiles cache from the normal Redis cache.
+    # django-pipeline.
+    # ------------------------------------------------------------------
     PIPELINE_COFFEE_SCRIPT_BINARY = '/app/bin/node /app/node_modules/coffee-script/bin/coffee'
     PIPELINE_YUGLIFY_BINARY = '/app/bin/node /app/node_modules/yuglify/bin/yuglify'
-    CACHES['staticfiles'] = {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'staticfiles',
-        # Long cache timeout for staticfiles, since this is used
-        # heavily by the optimizing storage.
-        'TIMEOUT': 60 * 60 * 24 * 365,
-    }
