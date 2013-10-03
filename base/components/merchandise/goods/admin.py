f57ecc7fce3ef3fa0694b43d9b897e0b158b2547
Bryan Veloso <bryan@revyver.com>
2013-10-02 18:39:56 -0700

    Some fieldsets.

--- base/components/merchandise/goods/admin.py
+++ base/components/merchandise/goods/admin.py
@@ -4,7 +4,24 @@ from .models import Good, Set, Shop, SuperSet
 
 
 class GoodAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': (('romanized_name', 'name'), ('price', 'link'), ('available_from', 'available_until'), 'image')}),
+        ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
+        ('Participants', {'fields': ('idols', 'groups')}),
+        ('Participants (Rendered)', {
+            'classes': ('grp-collapse grp-closed',),
+            'description': 'This is calculated by the values inputted in "Participants."',
+            'fields': ('participating_idols', 'participating_groups')
+        })
+        # (None, {'fields': ('',)})
+    )
+    readonly_fields = ['participating_idols', 'participating_groups']
+
+    raw_id_fields = ('event', 'idols', 'groups', 'shop',)
+    autocomplete_lookup_fields = {
+        'fk': ['event', 'shop'],
+        'm2m': ['idols', 'groups']
+    }
 admin.site.register(Good, GoodAdmin)
 
 
