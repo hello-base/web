c465fd03a0e9210337195d73035703adabcd3636
Bryan Veloso <bryan@revyver.com>
2013-10-02 18:45:10 -0700

    Subclasses.

--- base/components/merchandise/goods/admin.py
+++ base/components/merchandise/goods/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Good, Set, Shop, SuperSet
 
 
-class GoodAdmin(admin.ModelAdmin):
+class BaseAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': (('romanized_name', 'name'), ('price', 'link'), ('available_from', 'available_until'), 'image')}),
         ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
@@ -22,6 +22,32 @@ class GoodAdmin(admin.ModelAdmin):
         'fk': ['event', 'shop'],
         'm2m': ['idols', 'groups']
     }
+
+
+class GoodAdmin(BaseAdmin):
+    fieldsets = (
+        (None, {'fields': (('romanized_name', 'name'), ('price', 'link'), ('available_from', 'available_until'), 'image')}),
+        ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
+        ('Participants', {'fields': ('idols', 'groups')}),
+        ('Participants (Rendered)', {
+            'classes': ('grp-collapse grp-closed',),
+            'description': 'This is calculated by the values inputted in "Participants."',
+            'fields': ('participating_idols', 'participating_groups')
+        })
+        # (None, {'fields': ('',)})
+    )
+    readonly_fields = ['participating_idols', 'participating_groups']
+
+    raw_id_fields = ('event', 'idols', 'groups', 'shop',)
+    autocomplete_lookup_fields = {
+        'fk': ['event', 'shop'],
+        'm2m': ['idols', 'groups']
+    }
+
+    def get_fieldsets(self, request, obj=None):
+        fs = super(GoodAdmin, self).get_fieldsets(request, obj)
+        # fs now contains [(None, {'fields': fields})], do with it whatever you want
+        return fs
 admin.site.register(Good, GoodAdmin)
 
 
