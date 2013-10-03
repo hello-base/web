8c17db3dbc2ab39c17c5106b4564f358d5acff80
Bryan Veloso <bryan@revyver.com>
2013-10-02 18:55:03 -0700

    Moar options.

--- base/components/merchandise/goods/admin.py
+++ base/components/merchandise/goods/admin.py
@@ -5,16 +5,18 @@ from .models import Good, Set, Shop, SuperSet
 
 class BaseAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {'fields': (('romanized_name', 'name'), ('price', 'link'), ('available_from', 'available_until'), 'image')}),
+        (None, {'fields': (('romanized_name', 'name'),)}),
+        ('Details', {'fields': (('price', 'link'), ('available_from', 'available_until'), 'image')}),
         ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
         ('Participants', {'fields': ('idols', 'groups')}),
         ('Participants (Rendered)', {
             'classes': ('grp-collapse grp-closed',),
             'description': 'This is calculated by the values inputted in "Participants."',
             'fields': ('participating_idols', 'participating_groups')
-        })
-        # (None, {'fields': ('',)})
+        }),
+        ('Options', {'fields': (('is_graduation_good', 'is_birthday_good'), ('is_online_exclusive', 'is_mailorder_exclusive'))})
     )
+    list_display = ['romanized_name', 'name', 'price', 'available_from', 'available_until', 'event', 'shop', 'online_id']
     readonly_fields = ['participating_idols', 'participating_groups']
 
     raw_id_fields = ('event', 'idols', 'groups', 'shop',)
@@ -38,11 +40,11 @@ class SetAdmin(BaseAdmin):
 admin.site.register(Set, SetAdmin)
 
 
-class ShopAdmin(BaseAdmin):
+class ShopAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Shop, ShopAdmin)
 
 
-class SuperSetAdmin(admin.ModelAdmin):
+class SuperSetAdmin(BaseAdmin):
     pass
 admin.site.register(SuperSet, SuperSetAdmin)
