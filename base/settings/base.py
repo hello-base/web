2169e8776ea9f912ee4cf453c50673ed8ed4fa4b
Bryan Veloso <bryan@revyver.com>
2013-10-03 12:16:48 -0700

    Commit what we have thus far... because this is a lot.

--- base/settings/base.py
+++ base/settings/base.py
@@ -199,7 +199,7 @@ class Base(Configuration):
     ]
     ECSTATIC_MANIFEST_FILE = join(DJANGO_ROOT, 'settings', 'manifest.json')
     STATICBUILDER_BUILD_ROOT = join(DJANGO_ROOT, 'build')
-    STATICBUILDER_BUILD_COMMANDS = ['inv yuglify']
+    STATICBUILDER_BUILD_COMMANDS = ['grunt deploy']
 
     # django-grappelli.
     # ------------------------------------------------------------------
