bb3ab45ca4da6638e94ce9e6251beb58a4a16138
Bryan Veloso <bryan@revyver.com>
2013-10-04 16:50:41 -0700

    Create a testing configuration.

---
+++ base/settings/testing.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import os
+
+from .base import Base as Settings
+
+
+class Testing(Settings):
+    # Database Configuration.
+    # ------------------------------------------------------------------
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'base-test'
+        }
+    }
