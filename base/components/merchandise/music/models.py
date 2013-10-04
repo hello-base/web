c03575f46e0d8a127e3f907610c4569f83eef6c0
Bryan Veloso <bryan@revyver.com>
2013-10-04 10:18:04 -0700

    Need the autocomplete methods on music.Video as well.

--- base/components/merchandise/music/models.py
+++ base/components/merchandise/music/models.py
@@ -255,6 +255,10 @@ class Video(models.Model):
     def __unicode__(self):
         return u'%s' % (self.romanized_name)
 
+    @staticmethod
+    def autocomplete_search_fields():
+        return ('id__iexact', 'name__icontains', 'romanized_name__icontains')
+
     @property
     def parent(self):
         return filter(None, [self.album, self.single])[0]
