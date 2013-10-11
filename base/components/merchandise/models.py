6561408374bea136a98be22215d391629fbee6ad
Bryan Veloso <bryan@revyver.com>
2013-10-11 15:44:48 -0700

    We're no longer abstracting. Good DVDs and stand-alones ARE different things.

--- base/components/merchandise/models.py
+++ base/components/merchandise/models.py
@@ -1,7 +1,5 @@
 from datetime import date
 
-from model_utils import Choices
-from model_utils.models import TimeStampedModel
 from ohashi.db import models
 
 from components.accounts.models import ContributorMixin
@@ -23,33 +21,3 @@ class Merchandise(ContributorMixin, ParticipationMixin):
 
     def __unicode__(self):
         return '%s' % self.name
-
-
-class Videodisc(Merchandise):
-    """
-    An abstract base class for videodisc merchandise. Because of the fundemental
-    difference between "concert" discs (which are released to the public) and
-    "fan club" discs (which are generally only released to fan club members),
-    we cannot consolidate the two into one class.
-
-    """
-    class Meta:
-        abstract = True
-
-
-class VideodiscFormat(models.Model):
-    """
-    An abstract base class for videodiscs. Since disc-based merchandise can
-    be released on more than one format, this class covers the specific format
-    metadata.
-
-    """
-    FORMAT_TYPES = Choices((1, 'dvd', 'DVD'), (2, 'bluray', 'Blu-ray Disc'))
-
-    # Metadata
-    kind = models.IntegerField(choices=FORMAT_TYPES, default=FORMAT_TYPES.dvd)
-    released = models.DateField(blank=True, db_index=True, default=date.min, null=True)
-    catalog_number = models.CharField(blank=True)
-
-    class Meta:
-        abstract = True
