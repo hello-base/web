5618eb5503616596c0efab82bdc9c04faf49d44a
Bryan Veloso <bryan@revyver.com>
2013-10-12 11:53:54 -0700

    Ack, I boo-boo'ed the script.

--- base/components/merchandise/utils.py
+++ base/components/merchandise/utils.py
@@ -1,13 +1,15 @@
-def uuid_encode(self, uuid):
+def uuid_encode(uuid):
     """
     Encodes a UUID into a string (LSB first) according to the alphabet
     If leftmost (MSB) bits 0, string might be shorter
 
     """
     alphabet = list('23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
+    alpha_len = len(alphabet)
+
     unique_id = uuid.int
     output = ''
     while unique_id:
-        unique_id, digit = divmod(unique_id, self._alpha_len)
-        output += self.alphabet[digit]
+        unique_id, digit = divmod(unique_id, alpha_len)
+        output += alphabet[digit]
     return output
