f79172a1b95d38c09407ac9e3e534dfd48644b78
Bryan Veloso <bryan@revyver.com>
2013-10-13 13:48:23 -0700

    Supply modified queries to be indexed by Haystack.

--- base/components/people/search_indexes.py
+++ base/components/people/search_indexes.py
@@ -12,6 +12,11 @@ class IdolIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Idol
 
+    def index_queryset(self, using=None):
+        qs = super(IdolIndex, self).index_queryset(using=using)
+        qs = qs.select_related('primary_membership__group')
+        return qs
+
 
 class GroupIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.NgramField(document=True, use_template=True)
