f7a5bcf82b9e94072d4e72d22deaac6a3cdf43d0
Bryan Veloso <bryan@revyver.com>
2013-10-15 23:51:23 -0700

    Start the context dataing.

--- base/components/audits/views.py
+++ base/components/audits/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class AuditListView(TemplateView):
     template_name = 'audits/audit_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AuditListView, self).get_context_data(**kwargs)
+        return context
