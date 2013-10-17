33ca02cfb7f2484297683e9b238c4529f30ee7c7
Bryan Veloso <bryan@revyver.com>
2013-10-17 16:54:38 -0700

    Modify the Elasticsearch backend extension.

--- base/components/search_backends.py
+++ base/components/search_backends.py
@@ -4,6 +4,8 @@ from haystack.backends.elasticsearch_backend import (ElasticsearchSearchBackend,
 
 
 class KuromojiElasticsearchBackend(ElasticsearchSearchBackend):
+    DEFAULT_ANALYZER = 'kuromoji_analyzer'
+
     def __init__(self, connection_alias, **connection_options):
         super(KuromojiElasticsearchBackend, self).__init__(connection_alias, **connection_options)
 
@@ -71,6 +73,18 @@ class KuromojiElasticsearchBackend(ElasticsearchSearchBackend):
             }
         }
         setattr(self, 'DEFAULT_SETTINGS', SETTINGS)
+
+    def build_schema(self, fields):
+        content_field_name, mapping = super(KuromojiElasticsearchBackend, self).build_schema(fields)
+
+        for field_name, field_class in fields.items():
+            field_mapping = mapping[field_class.index_fieldname]
+
+            if field_mapping['type'] == 'string' and field_class.indexed:
+                if not hasattr(field_class, 'facet_for') and not field_class.field_type in('ngram', 'edge_ngram'):
+                    field_mapping['analyzer'] = getattr(field_class, 'analyzer', self.DEFAULT_ANALYZER)
+            mapping[field_class.index_fieldname] = field_mapping
+        return (content_field_name, mapping)
 
 
 class KuromojiElastcisearchEngine(ElasticsearchSearchEngine):
