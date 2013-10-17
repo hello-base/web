c8f78b49ef8eeb4c90391790a9b92c3b3b213a00
Bryan Veloso <bryan@revyver.com>
2013-10-17 00:52:53 -0700

    (Successfully!) Write a test for the get_previous/_next methods.

--- tests/merchandise/music/test_models.py
+++ tests/merchandise/music/test_models.py
@@ -1,8 +1,10 @@
+import datetime
 import pytest
 
 from components.merchandise.music.models import Album, Single
 from components.merchandise.music.factories import (AlbumFactory,
     BaseFactory, SingleFactory)
+from components.people.factories import GroupFactory
 
 
 @pytest.mark.django_db
@@ -19,3 +21,21 @@ def test_single_factory():
     assert isinstance(factory, Single)
     assert 'single' in factory.romanized_name
     assert factory.identifier == 'single'
+
+
+@pytest.mark.django_db
+def test_single_get_previous_and_next():
+    single1 = SingleFactory(number='1', romanized_name='single1', released=datetime.date.today() - datetime.timedelta(days=14))
+    single2 = SingleFactory(number='2', romanized_name='single2', released=datetime.date.today() - datetime.timedelta(days=7))
+
+    group = GroupFactory()
+    single1.groups.add(group)
+    assert single1.groups.exists()
+    single2.groups.add(group)
+    assert single2.groups.exists()
+
+    previous = single2.get_previous
+    assert previous == single1
+
+    next = single1.get_next
+    assert next == single2
