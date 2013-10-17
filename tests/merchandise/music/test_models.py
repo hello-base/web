e683b16a8d0cbcd53c95ec75b31d2a1cc3dc3cb2
Bryan Veloso <bryan@revyver.com>
2013-10-17 01:06:59 -0700

    Encase tests in classes... and have it work this time.

--- tests/merchandise/music/test_models.py
+++ tests/merchandise/music/test_models.py
@@ -9,45 +9,44 @@ from components.people.factories import GroupFactory
 
 pytestmark = pytest.mark.django_db
 
-def test_album_factory():
-    factory = AlbumFactory()
-    assert isinstance(factory, Album)
-    assert 'album' in factory.romanized_name
-    assert factory.identifier == 'album'
+class TestAlbums:
+    def test_album_factory(self):
+        factory = AlbumFactory()
+        assert isinstance(factory, Album)
+        assert 'album' in factory.romanized_name
+        assert factory.identifier == 'album'
+
+    def test_album_get_absolute_url(self, client):
+        factory = AlbumFactory()
+        response = client.get(factory.get_absolute_url())
+        assert response.status_code == 200
 
 
-def test_album_get_absolute_url(client):
-    factory = AlbumFactory()
-    response = client.get(factory.get_absolute_url())
-    assert response.status_code == 200
+class TestSingles:
+    def test_single_factory(self):
+        factory = SingleFactory()
+        assert isinstance(factory, Single)
+        assert 'single' in factory.romanized_name
+        assert factory.identifier == 'single'
 
+    def test_single_get_absolute_url(self, client):
+        factory = SingleFactory()
+        response = client.get(factory.get_absolute_url())
+        assert response.status_code == 200
 
-def test_single_factory():
-    factory = SingleFactory()
-    assert isinstance(factory, Single)
-    assert 'single' in factory.romanized_name
-    assert factory.identifier == 'single'
+    def test_single_get_previous_and_next(self):
+        single1 = SingleFactory(romanized_name='single1', released=datetime.date.today() - datetime.timedelta(days=14), number='1')
+        single2 = SingleFactory(romanized_name='single2', released=datetime.date.today() - datetime.timedelta(days=7), number='2')
+        single3 = SingleFactory(romanized_name='single2', released=datetime.date.today())
 
+        group = GroupFactory()
+        single1.groups.add(group)
+        assert single1.groups.exists()
+        single2.groups.add(group)
+        assert single2.groups.exists()
+        single3.groups.add(group)
+        assert single2.groups.exists()
 
-def test_single_get_absolute_url(client):
-    factory = SingleFactory()
-    response = client.get(factory.get_absolute_url())
-    assert response.status_code == 200
-
-
-def test_single_get_previous_and_next():
-    single1 = SingleFactory(romanized_name='single1', released=datetime.date.today() - datetime.timedelta(days=14), number='1')
-    single2 = SingleFactory(romanized_name='single2', released=datetime.date.today() - datetime.timedelta(days=7), number='2')
-    single3 = SingleFactory(romanized_name='single2', released=datetime.date.today())
-
-    group = GroupFactory()
-    single1.groups.add(group)
-    assert single1.groups.exists()
-    single2.groups.add(group)
-    assert single2.groups.exists()
-    single3.groups.add(group)
-    assert single2.groups.exists()
-
-    assert single1.get_next == single2
-    assert single2.get_previous == single1
-    assert single2.get_next != single3
+        assert single1.get_next == single2
+        assert single2.get_previous == single1
+        assert single2.get_next != single3
