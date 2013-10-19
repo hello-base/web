8bdb9b838e8b3099b1beb79346b0b24b34358737
Bryan Veloso <bryan@revyver.com>
2013-10-18 20:21:52 -0700

    Rename original_track's related_name to "children."

--- base/components/merchandise/music/models.py
+++ base/components/merchandise/music/models.py
@@ -233,7 +233,7 @@ class Track(ParticipationMixin):
     translated_name = models.CharField(blank=True)
 
     # Alternate Versions.
-    original_track = models.ForeignKey('self', blank=True, null=True, related_name='parent',
+    original_track = models.ForeignKey('self', blank=True, null=True, related_name='children',
         help_text='If this track is a cover or alternate, choose the original track it\'s based off of.')
     is_cover = models.BooleanField('cover?', default=False)
     is_alternate = models.BooleanField('alternate?', default=False)
