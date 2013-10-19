3f76b2609bd43feacee9d134c375347ba1f2abf9
Bryan Veloso <bryan@revyver.com>
2013-10-19 11:53:36 -0700

    Such prose.

--- base/components/people/views.py
+++ base/components/people/views.py
@@ -23,11 +23,13 @@ class GroupDetailView(QuicklinksMixin, DetailView):
             'active': [m for m in memberships if m.ended is None],
             'active_count': len([m for m in memberships if m.ended is None]),
             'inactive': [m for m in memberships if m.ended],
+            'inactive_count': len([m for m in memberships if m.ended]),
+            'leader': get_object_or_none(Membership.objects.select_related('idol'), group=self.object.pk, ended__isnull=True, is_leader=True),
+            'leaders': sorted([m for m in memberships if m.ended and m.is_leader and m.leadership_started != None], key=attrgetter('leadership_started')),
+            'leader_count': len([m for m in memberships if m.ended and m.is_leader and m.leadership_started != None]),
 
             # 'active': [m for m in memberships if m.ended is None and m.is_leader == False],
             # 'inactive': [m for m in memberships if m.ended and m.is_leader == False],
-            # 'leader': get_object_or_none(Membership.objects.select_related('idol'), group=self.object.pk, ended__isnull=True, is_leader=True),
-            # 'leaders': sorted([m for m in memberships if m.ended and m.is_leader and m.leadership_started != None], key=attrgetter('leadership_started')),
         }
 
         context['albums'] = self.object.albums.prefetch_related('editions', 'participating_idols', 'participating_groups')
