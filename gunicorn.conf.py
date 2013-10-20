b3e24825ead2874bb086d6f8450c4935e4e29c78
Bryan Veloso <bryan@revyver.com>
2013-10-19 19:14:18 -0700

    Let's try using async workers again...

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,3 +5,9 @@ import os
 bind = '0.0.0.0:{}'.format(os.environ['PORT'])
 preload = True
 workers = multiprocessing.cpu_count() * 2
+worker_class = 'gevent'
+
+def post_fork(server, worker):
+    from psycogreen.gevent import patch_psycopg
+    patch_psycopg()
+    worker.log.info('Colored psycopg green.')
