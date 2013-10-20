ad0b0bb3ef636047fcab8c7b477bafd5b350fd45
Bryan Veloso <bryan@revyver.com>
2013-10-20 12:55:28 -0700

    Flesh out the goods admin. Be verbose about it.

--- base/components/merchandise/goods/admin.py
+++ base/components/merchandise/goods/admin.py
@@ -4,18 +4,6 @@ from .models import Good, Set, Shop, SuperSet
 
 
 class BaseAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (None, {'fields': (('romanized_name', 'name'),)}),
-        ('Details', {'fields': (('price', 'link'), ('available_from', 'available_until'), 'image')}),
-        ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
-        ('Participants', {'fields': ('idols', 'groups')}),
-        ('Participants (Rendered)', {
-            'classes': ('grp-collapse grp-closed',),
-            'description': 'This is calculated by the values inputted in "Participants."',
-            'fields': ('participating_idols', 'participating_groups')
-        }),
-        ('Options', {'fields': (('is_graduation_good', 'is_birthday_good'), ('is_online_exclusive', 'is_mailorder_exclusive'))})
-    )
     list_display = ['romanized_name', 'name', 'price', 'available_from', 'available_until', 'event', 'shop', 'online_id']
     readonly_fields = ['participating_idols', 'participating_groups']
 
@@ -27,16 +15,50 @@ class BaseAdmin(admin.ModelAdmin):
 
 
 class GoodAdmin(BaseAdmin):
-    def get_fieldsets(self, request, obj=None):
-        fs = super(GoodAdmin, self).get_fieldsets(request, obj)
-        # fs now contains [(None, {'fields': fields})], do with it whatever you want
-        # all_fields = fs[0][1]['fields']
-        return fs
+    fieldsets = (
+        (None, {'fields': ('parent', 'category')}),
+        (None, {'fields': (('romanized_name', 'name'),)}),
+        ('Details', {'fields': (('available_from', 'available_until'), ('price', 'link'), 'image')}),
+        ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
+        ('Participants', {'fields': ('idols', 'groups')}),
+        ('Participants (Rendered)', {
+            'classes': ('grp-collapse grp-closed',),
+            'description': 'This is calculated by the values inputted in "Participants."',
+            'fields': ('participating_idols', 'participating_groups')
+        }),
+        ('Options', {'fields': (
+            ('is_bonus_good', 'is_campaign_good'),
+            ('is_lottery_good', 'is_set_exclusive'),
+            ('is_graduation_good', 'is_birthday_good'),
+            ('is_online_exclusive', 'is_mailorder_exclusive'),
+        )})
+    )
+
+    raw_id_fields = ('event', 'idols', 'groups', 'shop', 'parent',)
+    autocomplete_lookup_fields = {
+        'fk': ['parent', 'event', 'shop'],
+        'm2m': ['idols', 'groups']
+    }
 admin.site.register(Good, GoodAdmin)
 
 
 class SetAdmin(BaseAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('category',)}),
+        (None, {'fields': (('romanized_name', 'name'),)}),
+        ('Details', {'fields': (('available_from', 'available_until'), ('price', 'link'), 'image')}),
+        ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
+        ('Participants', {'fields': ('idols', 'groups')}),
+        ('Participants (Rendered)', {
+            'classes': ('grp-collapse grp-closed',),
+            'description': 'This is calculated by the values inputted in "Participants."',
+            'fields': ('participating_idols', 'participating_groups')
+        }),
+        ('Options', {'fields': (
+            ('is_graduation_good', 'is_birthday_good'),
+            ('is_online_exclusive', 'is_mailorder_exclusive'),
+        )})
+    )
 admin.site.register(Set, SetAdmin)
 
 
@@ -47,5 +69,26 @@ admin.site.register(Shop, ShopAdmin)
 
 
 class SuperSetAdmin(BaseAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('goods', 'sets',)}),
+        (None, {'fields': (('romanized_name', 'name'),)}),
+        ('Details', {'fields': (('available_from', 'available_until'), ('price', 'link'), 'image')}),
+        ('Sources', {'fields': ('event', 'shop', 'online_id', 'other_info')}),
+        ('Participants', {'fields': ('idols', 'groups')}),
+        ('Participants (Rendered)', {
+            'classes': ('grp-collapse grp-closed',),
+            'description': 'This is calculated by the values inputted in "Participants."',
+            'fields': ('participating_idols', 'participating_groups')
+        }),
+        ('Options', {'fields': (
+            ('is_graduation_good', 'is_birthday_good'),
+            ('is_online_exclusive', 'is_mailorder_exclusive'),
+        )})
+    )
+
+    raw_id_fields = ('goods', 'sets', 'event', 'idols', 'groups', 'shop',)
+    autocomplete_lookup_fields = {
+        'fk': ['event', 'shop'],
+        'm2m': ['goods', 'sets', 'idols', 'groups']
+    }
 admin.site.register(SuperSet, SuperSetAdmin)
