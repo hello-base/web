945ff27d5208ac7a5ba5f1cc8f4e669cfd6937bc
Bryan Veloso <bryan@revyver.com>
2013-10-21 13:54:09 -0700

    Photo serializers.

--- base/components/api/serializers.py
+++ base/components/api/serializers.py
@@ -4,10 +4,16 @@ from components.people.models import Group, Idol
 
 
 class IdolSerializer(serializers.ModelSerializer):
+    photo = serializers.Field('photo.url')
+    photo_thumbnail = serializers.Field('photo_thumbnail.url')
+
     class Meta:
         model = Idol
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    photo = serializers.Field('photo.url')
+    photo_thumbnail = serializers.Field('photo_thumbnail.url')
+
     class Meta:
         model = Group
