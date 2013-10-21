e570c18955f28f9f3f4bbb28018b4a338ef3b13a
Bryan Veloso <bryan@revyver.com>
2013-10-21 13:54:00 -0700

    Basic detail views.

--- base/components/api/urls.py
+++ base/components/api/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, url, include
 
-from .views import GroupList, IdolList
+from .views import GroupList, GroupDetail, IdolList, IdolDetail
 
 
 group_urls = patterns('',
-    url(r'^$', GroupList.as_view(), name='group-list'),
+    url(r'^/(?P<slug>[-\w]+)$', name='group-detail', view=GroupDetail.as_view()),
+    url(r'^$', name='group-list', view=GroupList.as_view()),
 )
 
 idol_urls = patterns('',
-    url(r'^$', IdolList.as_view(), name='idol-list'),
+    url(r'^/(?P<slug>[-\w]+)$', name='idol-detail', view=IdolDetail.as_view()),
+    url(r'^$', name='idol-list', view=IdolList.as_view()),
 )
 
 urlpatterns = patterns('',
--- base/components/api/views.py
+++ base/components/api/views.py
@@ -10,6 +10,18 @@ class GroupList(generics.ListAPIView):
     serializer_class = GroupSerializer
 
 
+class GroupDetail(generics.RetrieveAPIView):
+    model = Group
+    serializer_class = GroupSerializer
+    lookup_field = 'slug'
+
+
 class IdolList(generics.ListAPIView):
     model = Idol
     serializer_class = IdolSerializer
+
+
+class IdolDetail(generics.RetrieveAPIView):
+    model = Idol
+    serializer_class = IdolSerializer
+    lookup_field = 'slug'
