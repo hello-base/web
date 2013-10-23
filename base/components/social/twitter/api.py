108c418fd797cc3abb5b1bcc054983eb583080ff
Bryan Veloso <bryan@revyver.com>
2013-10-23 11:00:34 -0700

    Wrapper around the wrapper for the Twitter API.

---
+++ base/components/social/twitter/api.py
@@ -0,0 +1,10 @@
+from twitter import Twitter, OAuth
+
+
+class Api:
+    service = Twitter(auth=OAuth(
+        settings.TWITTER_OAUTH_TOKEN,
+        settings.TWITTER_OAUTH_SECRET,
+        settings.TWITTER_CONSUMER_KEY,
+        settings.TWITTER_CONSUMER_SECRET
+    ))
