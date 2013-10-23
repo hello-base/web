6d4aacf90fccf1c20f6c5eb0780dfee79dcb74ad
Bryan Veloso <bryan@revyver.com>
2013-10-23 13:08:15 -0700

    Flesh out the Twitters.

--- base/components/social/twitter/models.py
+++ base/components/social/twitter/models.py
@@ -10,7 +10,7 @@ class TwitterUser(models.Model):
     name = models.CharField(blank=True, max_length=200)
     description = models.TextField(blank=True)
     location = models.CharField(blank=True, max_length=200)
-    avatar = models.URLField(blank=True)
+    profile_image_url = models.URLField(blank=True)
     url = models.URLField(blank=True)
 
     # Optional relationships.
@@ -25,4 +25,37 @@ class TwitterUser(models.Model):
 
 
 class Tweet(models.Model):
-    pass
+    user = models.ForeignKey(TwitterUser, related_name='tweets')
+
+    tweet_id = models.BigIntegerField(blank=True, null=True)
+    tweet_id_str = models.CharField(blank=True, max_length=200)
+    created_at = models.DateTimeField(blank=True)
+    text = models.TextField(blank=True)
+    source = models.CharField(blank=True, max_length=200)
+
+    # Replies.
+    in_reply_to_user_id = models.BigIntegerField(blank=True, null=True)
+    in_reply_to_user_id_str = models.CharField(blank=True)
+    in_reply_to_status_id = models.BigIntegerField(blank=True, null=True)
+    in_reply_to_status_id_str = models.CharField(blank=True)
+
+    # Retweets.
+    retweeted = models.BooleanField(default=False)
+    retweeter_profile_image_url = models.URLField(blank=True)
+    retweeter_screen_name = models.CharField(blank=True)
+    retweeter_name = models.CharField(blank=True)
+    retweeted_status_id = models.BigIntegerField(blank=True, null=True)
+    retweeted_status_id_str = models.CharField(blank=True)
+    retweeted_status_created_at = models.DateTimeField(blank=True, null=True)
+    retweeted_status_text = models.TextField(blank=True)
+    retweeted_status_source = models.CharField(blank=True, max_length=200)
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.user.screen_name, self.text)
+
+    def get_absolute_url(self):
+        parts = {'screen_name': self.user.screen_name, 'id': self.tweet_id_str}
+        if self.retweeted:
+            parts['screen_name'] = self.retweeter_screen_name
+            parts['id'] = self.retweeted_status_id_str
+        return 'https://twitter.com/%s/status/%s' % (parts['screen_name'], parts['id'])
