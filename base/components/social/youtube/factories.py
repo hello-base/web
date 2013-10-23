fcdc149803875235fcefc430f7d3c5e1cd424b99
Bryan Veloso <bryan@revyver.com>
2013-10-23 16:25:37 -0700

    Add factories for the YouTube models.

---
+++ base/components/social/youtube/factories.py
@@ -0,0 +1,12 @@
+import datetime
+import factory
+
+from . import models
+
+
+class ChannelFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = models.Channel
+
+
+class VideoFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = models.Video
