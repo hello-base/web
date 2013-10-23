c390306d6858ecd10d5f68b7eb69cb15c4189284
Bryan Veloso <bryan@revyver.com>
2013-10-23 16:25:49 -0700

    Basic tests for the YouTube bits.

---
+++ tests/social/youtube/test_models.py
@@ -0,0 +1,19 @@
+import pytest
+
+from components.social.youtube.models import Channel, Video
+from components.social.youtube.factories import ChannelFactory, VideoFactory
+
+
+pytestmark = pytest.mark.django_db
+
+class TestChannels:
+    def test_channel_factory(self):
+        factory = ChannelFactory()
+        assert isinstance(factory, Channel)
+
+
+class TestVideo:
+    def test_video_factory(self):
+        channel = ChannelFactory()
+        factory = VideoFactory(channel=channel)
+        assert isinstance(factory, Video)
