e3e8393e7353c83f382e159e5107a7c2e1c47f26
Bryan Veloso <bryan@revyver.com>
2013-10-23 11:59:10 -0700

    Some API methods.

--- base/components/social/twitter/api.py
+++ base/components/social/twitter/api.py
@@ -8,3 +8,12 @@ class Api:
         settings.TWITTER_CONSUMER_KEY,
         settings.TWITTER_CONSUMER_SECRET
     ))
+
+    def fetch_timeline_by_screen_name(self, screen_name):
+        return Api.service.statuses.user_timeline(screen_name=screen_name)
+
+    def fetch_user_by_screen_name(self, screen_name):
+        return Api.service.users.show(screen_name=screen_name)
+
+    def fetch_user_by_twitter_id(self, twitter_id):
+        return Api.service.users.show(twitter_id=twitter_id)
