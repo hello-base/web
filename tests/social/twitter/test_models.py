6ec8191797c8e57ecfc3fcb34fc68396536dab9d
Bryan Veloso <bryan@revyver.com>
2013-10-23 16:22:13 -0700

    Simple tests.

--- tests/social/twitter/test_models.py
+++ tests/social/twitter/test_models.py
@@ -7,12 +7,13 @@ from components.social.twitter.factories import TweetFactory, TwitterUserFactory
 pytestmark = pytest.mark.django_db
 
 class TestTwitterUsers:
-    def test_twitteruser_factory():
+    def test_twitteruser_factory(self):
         factory = TwitterUserFactory()
         assert isinstance(factory, TwitterUser)
 
 
 class TestTweets:
-    def test_twitteruser_factory():
-        factory = TweetFactory()
+    def test_tweet_factory(self):
+        user = TwitterUserFactory()
+        factory = TweetFactory(user=user)
         assert isinstance(factory, Tweet)
