744c2c1dde1a595d97670147391bd5d0c329549d
Bryan Veloso <bryan@revyver.com>
2013-10-23 19:51:53 -0700

    Test more plain views.

--- tests/test_views.py
+++ tests/test_views.py
@@ -7,3 +7,15 @@ def test_site_view(client):
     response = client.get(reverse('site-home'))
     assert response.status_code == 200
     assert 'landings/home_site.html' in [template.name for template in response.templates]
+
+
+def test_plain_text_view(client):
+    response = client.get(reverse('humans'))
+    assert response.status_code == 200
+    assert 'humans.txt' in [template.name for template in response.templates]
+
+
+def test_xml_view(client):
+    response = client.get(reverse('opensearch'))
+    assert response.status_code == 200
+    assert 'opensearch.xml' in [template.name for template in response.templates]
