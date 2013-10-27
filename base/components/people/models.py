b480bea61591eb795bfe1dd2c474680435430304
Bryan Veloso <bryan@revyver.com>
2013-10-27 10:53:25 -0700

    Remove another explicit False.

--- base/components/people/models.py
+++ base/components/people/models.py
@@ -250,7 +250,6 @@ class Membership(models.Model):
     def is_active(self):
         if self.ended is None or self.ended >= date.today():
             return True
-        return False
 
     def days_before_starting(self):
         return (self.started - self.group.started).days
