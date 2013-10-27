d799c9f4bafae79b0355f5f8c11b416d252373f5
Bryan Veloso <bryan@revyver.com>
2013-10-26 21:31:08 -0700

    Add "Facts." Fixes #43.

--- base/components/people/models.py
+++ base/components/people/models.py
@@ -370,3 +370,16 @@ class Headshot(models.Model):
             self.idol.photo = latest.photo
             self.idol.photo_thumbnail = latest.photo_thumbnail
             self.idol.save()
+
+
+class Fact(models.Model):
+    idol = models.ForeignKey(Idol, blank=True, null=True, related_name='facts')
+    group = models.ForeignKey(Group, blank=True, null=True, related_name='facts')
+    body = models.TextField()
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.parent.romanized_name, self.body[:30])
+
+    @property
+    def parent(self):
+        return filter(None, [self.album, self.single])[0]
