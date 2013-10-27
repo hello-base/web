fffef63e1bf4b0a2c5c153bd50885388cffa5023
Bryan Veloso <bryan@revyver.com>
2013-10-27 14:02:40 -0700

    Editor factory and super-basic tests.

---
+++ tests/accounts/test_models.py
@@ -0,0 +1,16 @@
+import pytest
+
+from components.accounts.factories import EditorFactory
+from components.accounts.models import Editor
+
+pytestmark = pytest.mark.django_db
+
+
+class TestEditors:
+    def test_factory(self):
+        factory = EditorFactory()
+        assert isinstance(factory, Editor)
+        assert 'dancer' in factory.username
+
+    def test_creation(self):
+        factory = EditorFactory()
