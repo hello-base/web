221fa0eced407a3b613d735ee0aabf0763bf939e
Bryan Veloso <bryan@revyver.com>
2013-10-26 18:18:10 -0700

    Reduce the complexity of the contextual_participants() inclusion tag.

--- base/components/merchandise/music/templatetags/music_tags.py
+++ base/components/merchandise/music/templatetags/music_tags.py
@@ -1,54 +1,58 @@
 from django import template
 register = template.Library()
 
-from components.people.constants import CLASSIFICATIONS
-
 
 @register.inclusion_tag('people/partials/idol_contexted_participants.html')
 def contextual_participants(release, context):
-    if context.identifier == 'idol':
-        return _render_idol_contextual_participants(release, context)
-    if context.identifier == 'group':
-        return _render_group_contextual_participants(release, context)
-
-
-def _render_idol_contextual_participants(release, idol):
-    groups = idol.groups.all()
     participants = release.participants
-    relationships = {'solo': False, 'soloist': False, 'for': [], 'with': []}
-    if len(participants) == 1:
-        for participant in participants:
-            if participant != idol.primary_membership.group and participant != idol:
-                relationships['for'].append(participant)
-            if participant == idol:
-                # We return an empty dictionary to tell the template
-                # tag that this is a solo work.
-                relationships['solo'] = True
-
-                # Hackity hack. We DO NOT want to show the "solo work"
-                # pill for memebers of the soloist group.
-                if 'Soloist' in str(idol.primary_membership):
-                    relationships['soloist'] = True
-
-                return {'relationships': relationships}
-    else:
-        for participant in participants:
-            if participant in groups:
-                relationships['for'].append(participant)
-
-                # Bait and switch, Python style. We don't want to
-                # plug primary groups or idols themselves into the
-                # dictionary, so we have to go back on our word
-                # and remove them from it.
-                if participant == idol.primary_membership.group or participant == idol:
-                    relationships['for'].remove(participant)
-            elif participant != idol:
-                relationships['with'].append(participant)
+    if context.identifier == 'idol':
+        try:
+            (participant,) = participants
+            relationships = parse_idol(participant, context)
+        except ValueError:
+            relationships = parse_idols(participants, context)
+    if context.identifier == 'group':
+        relationships = parse_groups(participants, context)
     return {'relationships': relationships}
 
 
-def _render_group_contextual_participants(release, group):
-    participants = release.participants
+def parse_idol(participant, idol):
+    relationships = {'for': []}
+    if participant != idol and participant != idol.primary_membership.group:
+        relationships['for'].append(participant)
+    if participant == idol:
+        # We return an empty dictionary to tell the template
+        # tag that this is a solo work.
+        relationships['solo'] = True
+
+        # Hackity hack. We DO NOT want to show the "solo work"
+        # pill for memebers of the soloist group.
+        if 'Soloist' in str(idol.primary_membership):
+            relationships['soloist'] = True
+    return relationships
+
+
+def parse_idols(participants, idol):
+    groups = idol.groups.all()
+    relationships = {'for': [], 'with': []}
+    for participant in participants:
+        if participant in groups:
+            relationships['for'].append(participant)
+
+            # Bait and switch, Python style. We don't want to
+            # plug primary groups or idols themselves into the
+            # dictionary, so we have to go back on our word
+            # and remove them from it.
+            if participant == idol.primary_membership.group or participant == idol:
+                relationships['for'].remove(participant)
+        elif participant != idol:
+            relationships['with'].append(participant)
+    return relationships
+
+
+def parse_groups(participants, group):
+    from components.people.constants import CLASSIFICATIONS
+
     relationships = {'for': [], 'with': []}
     for participant in participants:
         if (hasattr(participant, 'classification')
@@ -57,7 +61,7 @@ def _render_group_contextual_participants(release, group):
             # We have a supergroup. That's all we need.
             # Also, H!P All Stars is a supergroup in my view! D:
             relationships['for'].append(participant)
-            return {'relationships': relationships}
+            return relationships
         elif participant != group:
             relationships['with'].append(participant)
-    return {'relationships': relationships}
+    return relationships
