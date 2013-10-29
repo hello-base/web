39d13612fb664b2fe3a856015c509e1ef34ddb58
Bryan Veloso <bryan@revyver.com>
2013-10-28 19:12:37 -0700

    Create slugs for Event and Venue factories.

--- base/components/events/factories.py
+++ base/components/events/factories.py
@@ -1,6 +1,8 @@
 import datetime
 import factory
 
+from django.template.defaultfilters import slugify
+
 from . import models
 
 
@@ -8,6 +10,7 @@ class EventFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = models.Event
 
     romanized_name = factory.Sequence(lambda i: 'event#%s' % i)
+    slug = factory.LazyAttribute(lambda f: slugify(f.romanized_name))
 
 
 class PerformanceFactory(factory.django.DjangoModelFactory):
@@ -22,3 +25,4 @@ class VenueFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = models.Venue
 
     romanized_name = factory.Sequence(lambda i: 'venue#%s' % i)
+    slug = factory.LazyAttribute(lambda f: slugify(f.romanized_name))
