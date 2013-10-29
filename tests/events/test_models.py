f7b4df4b4574bc3ae56136e1a45d28fc191b711c
Bryan Veloso <bryan@revyver.com>
2013-10-28 19:01:52 -0700

    Basic tests for events.

---
+++ tests/events/test_models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import datetime
+import pytest
+
+from components.events.models import Event, Performance, Venue
+from components.events.factories import (EventFactory, PerformanceFactory,
+    VenueFactory)
+
+pytestmark = pytest.mark.django_db
+
+
+class TestEvents:
+    def test_factory(self):
+        factory = EventFactory()
+        assert isinstance(factory, Event)
+        assert 'event' in factory.romanized_name
+
+
+class TestPerformances:
+    def test_factory(self):
+        factory = PerformanceFactory()
+        assert isinstance(factory, Performance)
+        assert 'performance' in factory.romanized_name
+
+
+class TestVenues:
+    def test_factory(self):
+        factory = VenueFactory()
+        assert isinstance(factory, Venue)
+        assert 'venue' in factory.romanized_name
