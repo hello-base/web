4fe25639cb9433d1b02da0c29ebc7526ce167aa3
Bryan Veloso <bryan@revyver.com>
2013-10-28 19:01:36 -0700

    Provide some fields for the event factories.

--- base/components/events/factories.py
+++ base/components/events/factories.py
@@ -7,10 +7,18 @@ from . import models
 class EventFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = models.Event
 
+    romanized_name = factory.Sequence(lambda i: 'event#%s' % i)
+
 
 class PerformanceFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = models.Performance
 
+    day = datetime.date.today()
+    event = factory.SubFactory(EventFactory)
+    romanized_name = factory.Sequence(lambda i: 'performance#%s' % i)
+
 
 class VenueFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = models.Venue
+
+    romanized_name = factory.Sequence(lambda i: 'venue#%s' % i)
