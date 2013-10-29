c83c8ccb4bfa8ef1841f98b7ffc5f324bb752078
Bryan Veloso <bryan@revyver.com>
2013-10-28 19:36:42 -0700

    Event view tests.

---
+++ tests/events/test_views.py
@@ -0,0 +1,23 @@
+import pytest
+
+from django.core.urlresolvers import reverse
+
+from components.events import factories
+
+
+@pytest.mark.django_db
+def test_event_detail_view(client):
+    group = factories.EventFactory()
+    response = client.get(reverse('event-detail', kwargs={'slug': group.slug}))
+    assert response.status_code == 200
+    assert 'object' in response.context
+    assert 'events/event_detail.html' in [template.name for template in response.templates]
+
+
+@pytest.mark.django_db
+def test_venue_detail_view(client):
+    idol = factories.VenueFactory()
+    response = client.get(reverse('venue-detail', kwargs={'slug': idol.slug}))
+    assert response.status_code == 200
+    assert 'object' in response.context
+    assert 'events/venue_detail.html' in [template.name for template in response.templates]
