4657ecdf6889684cf83c77f34233d8bd3ba852a2
Bryan Veloso <bryan@revyver.com>
2013-10-28 19:14:33 -0700

    Test string representations and get_absolute_url() calls.

--- tests/events/test_models.py
+++ tests/events/test_models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 import pytest
 
 from components.events.models import Event, Performance, Venue
@@ -14,6 +15,11 @@ class TestEvents:
         assert isinstance(factory, Event)
         assert 'event' in factory.romanized_name
 
+    def test_get_absolute_url(self, client):
+        factory = EventFactory()
+        response = client.get(factory.get_absolute_url())
+        assert response.status_code == 200
+
 
 class TestPerformances:
     def test_factory(self):
@@ -21,9 +27,27 @@ class TestPerformances:
         assert isinstance(factory, Performance)
         assert 'performance' in factory.romanized_name
 
+    def test_string_representation(self):
+        day = datetime.date.today()
+        performance = PerformanceFactory()
+        assert str(day) in str(performance)
+
+        start_time = datetime.datetime.now().time()
+        performance_with_start_time = PerformanceFactory(start_time=start_time)
+        assert str(start_time) in str(performance_with_start_time)
+
 
 class TestVenues:
     def test_factory(self):
         factory = VenueFactory()
         assert isinstance(factory, Venue)
         assert 'venue' in factory.romanized_name
+
+    def test_get_absolute_url(self, client):
+        factory = VenueFactory()
+        response = client.get(factory.get_absolute_url())
+        assert response.status_code == 200
+
+    def test_string_representation(self):
+        factory = VenueFactory()
+        assert str(factory) == factory.romanized_name
