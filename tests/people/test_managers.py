d4c8b0f15cd1694b84f8dab7936571d9f9bca42f
Bryan Veloso <bryan@revyver.com>
2013-10-30 01:36:22 -0700

    Test group managers. Rename idols() => status().

--- tests/people/test_managers.py
+++ tests/people/test_managers.py
@@ -1,22 +1,41 @@
+# -*- coding: utf-8 -*-
+import datetime
 import pytest
 
-from components.people.factories import IdolFactory
-from components.people.models import Idol
 from components.people.constants import STATUS
+from components.people.factories import GroupFactory, IdolFactory
+from components.people.models import Group, Idol
 
 pytestmark = pytest.mark.django_db
 
 
 class TestIdols:
     @pytest.fixture
-    def idols(self):
+    def status(self):
         idols = []
         [idols.append(IdolFactory(status=STATUS.active)) for i in xrange(3)]
         [idols.append(IdolFactory(status=STATUS.former)) for i in xrange(2)]
         return idols
 
-    def test_active_manager(self, idols):
+    def test_active_manager(self, status):
         assert len(Idol.objects.active()) == 3
 
-    def test_inactive_manager(self, idols):
+    def test_inactive_manager(self, status):
         assert len(Idol.objects.inactive()) == 2
+
+
+class TestGroups:
+    @pytest.fixture
+    def status(self):
+        groups = [
+            GroupFactory(),
+            GroupFactory(ended=datetime.date.today() + datetime.timedelta(days=30)),
+            GroupFactory(ended=datetime.date.today() - datetime.timedelta(days=30))
+        ]
+        return groups
+
+    def test_active_manager(self, status):
+        assert len(Group.objects.active()) == 2
+
+    def test_inactive_manager(self, status):
+        assert len(Group.objects.inactive()) == 1
