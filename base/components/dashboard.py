25e725b940bbb2ab206b9d43d086750763a7a5d8
Bryan Veloso <bryan@revyver.com>
2013-10-30 15:34:23 -0700

    Revert to eae8f6495.

--- base/components/dashboard.py
+++ base/components/dashboard.py
@@ -4,23 +4,29 @@ from grappelli.dashboard import modules, Dashboard
 class CustomIndexDashboard(Dashboard):
     def init_with_context(self, context):
         # Hello! Base core modules.
-        self.children.append(modules.AppList(
-            title='All', column=1, collapsible=False,
-            models=('components.*',),
+        self.children.append(modules.ModelList(
+            title='Idols & Staff', column=1, collapsible=False,
+            models=('components.people.*',),
         ))
-
-        # self.children.append(modules.ModelList(
-        #     title='Idols & Staff', column=1, collapsible=False,
-        #     models=('components.people.*',),
-        # ))
-        # self.children.append(modules.ModelList(
-        #     title='Events', column=1, collapsible=False,
-        #     models=('components.events.*',),
-        # ))
-        # self.children.append(modules.AppList(
-        #     title='Merchandise', column=1, collapsible=False,
-        #     models=('components.merchandise.*',),
-        # ))
+        self.children.append(modules.ModelList(
+            title='Events', column=1, collapsible=False,
+            models=('components.events.*',),
+        ))
+        self.children.append(modules.AppList(
+            title='Merchandise', column=1, collapsible=False,
+            models=('components.merchandise.*',),
+        ))
+        self.children.append(modules.AppList(
+            title='Music', column=1, collapsible=False,
+            models=(
+                'components.merchandise.music.models.Album',
+                'components.merchandise.music.models.Single',
+                'components.merchandise.music.models.Editions',
+                'components.merchandise.music.models.Labels',
+                'components.merchandise.music.models.Tracks',
+                'components.merchandise.music.models.Videos',
+            ),
+        ))
 
         # Recent actions.
         self.children.append(modules.RecentActions(
