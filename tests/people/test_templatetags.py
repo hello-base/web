b24fedd08485b89521747599c36ba8d4a54f9ed0
Bryan Veloso <bryan@revyver.com>
2013-10-31 11:36:19 -0700

    " => '

--- tests/people/test_templatetags.py
+++ tests/people/test_templatetags.py
@@ -6,7 +6,7 @@ from django.template import Template, Context
 def test_age():
     birthdate = datetime.date.today() - datetime.timedelta(days=731)
     out = Template(
-        "{% load people_tags %}"
-        "{{ birthdate|age }}"
+        '{% load people_tags %}'
+        '{{ birthdate|age }}'
     ).render(Context({'birthdate': birthdate}))
     assert out == '2'
