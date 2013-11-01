dc053258a86ec7ae07f2abfdf36045cfad95f304
Bryan Veloso <bryan@revyver.com>
2013-11-01 15:11:02 -0700

    Test the twitter task. This is pretty shallow, tbh. :(

---
+++ tests/social/twitter/test_tasks.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from components.social.twitter.factories import TwitterUserFactory, TweetFactory
+from components.social.twitter.tasks import fetch_tweets
+
+pytestmark = pytest.mark.django_db
+
+
+def test_fetch_tweets():
+    tweeter = TwitterUserFactory(screen_name='hello_base')
+    fetch_tweets()
+    assert tweeter.tweets.count > 0
