d8642923e437dc6587d87809c9dfd131e3c2cb0f
Bryan Veloso <bryan@revyver.com>
2013-11-06 14:31:06 -0800

    Add LocaleMiddleware.

--- base/settings/production.py
+++ base/settings/production.py
@@ -22,9 +22,10 @@ class Production(Settings):
     MIDDLEWARE_CLASSES = (
         'django.middleware.cache.UpdateCacheMiddleware',
         'django.middleware.gzip.GZipMiddleware',
-        'django.middleware.common.CommonMiddleware',
         'djangosecure.middleware.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.locale.LocaleMiddleware',
+        'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
