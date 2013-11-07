214a0c70d973e31b08ba708750c07741ed04a8a4
Bryan Veloso <bryan@revyver.com>
2013-11-07 15:11:57 -0800

    Strengthen the test.

--- tests/history/test_models.py
+++ tests/history/test_models.py
@@ -15,4 +15,7 @@ class TestHistories:
     def test_deltas(self):
         history1 = HistoryFactory(sum=10)
         history2 = HistoryFactory(sum=20)
+        history3 = HistoryFactory(sum=80)
+        assert history1.delta == 0
         assert history2.delta == 10
+        assert history3.delta == 60
