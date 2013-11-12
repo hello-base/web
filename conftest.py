a169ea30f6c5d469008029319150707358f298e5
Bryan Veloso <bryan@revyver.com>
2013-11-12 13:43:13 -0800

    Force the initialization of django-configurations in testing.

--- conftest.py
+++ conftest.py
@@ -2,14 +2,17 @@ import os
 
 from django import get_version
 
+from configurations import importer
+from dotenv import read_dotenv
+
 
 def pytest_report_header(config):
     return 'django: ' + get_version()
 
 
-def pytest_configure():
-    import dotenv
+def pytest_configure(config):
     dotenv.read_dotenv()
 
     os.environ['DJANGO_SETTINGS_MODULE'] = 'base.settings'
     os.environ['DJANGO_CONFIGURATION'] = 'Testing'
+    importer.install()
