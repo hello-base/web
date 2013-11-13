9ea39784511ee0ea468160323504bdf95280e5d3
Bryan Veloso <bryan@revyver.com>
2013-11-12 18:45:44 -0800

    "/media/" is used… change it.

--- base/components/merchandise/media/urls.py
+++ base/components/merchandise/media/urls.py
@@ -9,7 +9,7 @@ from .views import VideodiscDetailView
 urlpatterns = patterns('',
     # MultiURL allows us to unite all of the media under a simpler URL.
     multiurl(
-        url(r'^media/(?P<slug>[-\w]+)/$', name='videodisc-detail', view=VideodiscDetailView.as_view()),
+        url(r'^discs/(?P<slug>[-\w]+)/$', name='videodisc-detail', view=VideodiscDetailView.as_view()),
         catch=(Http404, ContinueResolving)
     ),
 )
