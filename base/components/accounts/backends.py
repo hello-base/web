97c66e1cbbc6fd691c2fec4f4e72ba22892fa13c
Bryan Veloso <bryan@revyver.com>
2013-11-12 16:23:14 -0800

    Remove the try/except clause from get_user().
    It doesn't seem like the code will -ever- hit the except clause as the
    method that calls this has fallbacks of its own.

--- base/components/accounts/backends.py
+++ base/components/accounts/backends.py
@@ -14,7 +14,4 @@ class HelloBaseIDBackend(ModelBackend):
             return user
 
     def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+        return User.objects.get(pk=user_id)
