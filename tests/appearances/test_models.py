bfd8b0b7e27571d073c13a72f46e1f8f13b3d62e
Bryan Veloso <bryan@revyver.com>
2013-11-12 20:40:14 -0800

    Unicode literals and fixing of the tests.

--- tests/appearances/test_models.py
+++ tests/appearances/test_models.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import pytest
+
+from django.utils.encoding import smart_text
 
 from components.appearances.factories import CardFactory
 from components.appearances.models import Card
@@ -10,29 +14,32 @@ pytestmark = pytest.mark.django_db
 
 class TestCards:
     def test_group_name(self):
-        group = GroupFactory(name=u'モーニング娘。')
+        group = GroupFactory(name='モーニング娘。')
         card1 = CardFactory(group=group)
-        card2 = CardFactory(other_group_name=u'AKB48')
-        assert card1.group_name == u'モーニング娘。'
-        assert card2.group_name == u'AKB48'
+        card2 = CardFactory(other_group_name='AKB48')
+        assert card1.group_name == 'モーニング娘。'
+        assert card2.group_name == 'AKB48'
 
     def test_romanized_group_name(self):
         group = GroupFactory(romanized_name='Morning Musume')
         card1 = CardFactory(group=group)
         card2 = CardFactory(other_group_romanized_name='AKB48')
-        assert card1.group_name == 'Morning Musume'
-        assert card2.group_name == 'AKB48'
+        assert card1.group_romanized_name == 'Morning Musume'
+        assert card2.group_romanized_name == 'AKB48'
 
     def test_model_name(self):
-        idol = GroupFactory(name=u'道重 さゆみ')
-        card1 = CardFactory(group=group)
-        card2 = CardFactory(other_model_name=u'秋元 才加')
-        assert card1.group_name == u'道重 さゆみ'
-        assert card2.group_name == u'秋元 才加'
+        model = IdolFactory(family_name='道重', given_name='さゆみ')
+        card1 = CardFactory(hp_model=model)
+        card2 = CardFactory(other_model_name='秋元才加')
+        assert card1.model_name == '道重さゆみ'
+        assert card2.model_name == '秋元才加'
 
     def test_romanized_model_name(self):
-        group = GroupFactory(name='Michishige Sayumi')
-        card1 = CardFactory(group=group)
+        model = IdolFactory(
+            family_name='道重', given_name='さゆみ',
+            romanized_family_name='Michishige', romanized_given_name='Sayumi'
+        )
+        card1 = CardFactory(hp_model=model)
         card2 = CardFactory(other_model_romanized_name='Akimoto Sayaka')
-        assert card1.group_name == 'Michishige Sayumi'
-        assert card2.group_name == 'Akimoto Sayaka'
+        assert card1.model_romanized_name == 'Michishige Sayumi'
+        assert card2.model_romanized_name == 'Akimoto Sayaka'
