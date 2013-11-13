3f105a4f584cf807d151ce6d6b43962370d7d8bd
Bryan Veloso <bryan@revyver.com>
2013-11-13 10:46:40 -0800

    Covert merchandise.Models from ohashi to native models.

--- base/components/merchandise/models.py
+++ base/components/merchandise/models.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from ohashi.db import models
+from django.db import models
 
 from components.accounts.models import ContributorMixin
 from components.people.models import ParticipationMixin
@@ -8,8 +8,8 @@ from components.people.models import ParticipationMixin
 
 class Merchandise(ContributorMixin, ParticipationMixin):
     # Shared metadata.
-    romanized_name = models.CharField()
-    name = models.CharField()
+    romanized_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     released = models.DateField(blank=True, db_index=True, default=date.min, null=True)
     price = models.IntegerField(blank=True, null=True)
 
