988f7f4d5cfc1b55b8e447669ff3d19115136369
Bryan Veloso <bryan@revyver.com>
2013-11-17 22:16:09 -0800

    Fix some oopsies.

--- base/components/correlations/models.py
+++ base/components/correlations/models.py
@@ -68,22 +68,23 @@ def record_correlation(sender, instance, **kwargs):
     if not instance._meta.model_name in MODELS:
         return
 
+    from components.people.models import Membership
+
     # Membership is a special case. Since most groups are static
     # (or non-generational), the date the group is formed is the same as
     # the date its members joined. So if those two values are equal, stop
     # the process.
-    if (isinstance(sender, Membership)
-        and instance.group is not None
+    if (instance._meta.model_name == 'membership'
         and instance.started == instance.group.started):
         return
     timestamp, attribute = call_attributes(instance, FIELDS)
     if timestamp is not None and timestamp != date.min:
         ctype = ContentType.objects.get_for_model(sender)
-        correlation, created = self.get_or_create(
+        correlation, created = Correlation.objects.get_or_create(
             content_type=ctype,
             object_id=smart_text(instance._get_pk_val()),
+            timestamp=timestamp,
             defaults={
-                'timestamp': timestamp,
                 'identifier': instance._meta.model_name,
                 'date_field': attribute,
                 'julian': timestamp.timetuple().tm_yday,
