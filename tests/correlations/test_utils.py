8307c3f1e580393ffe433ce10d84a09273be11f5
Bryan Veloso <bryan@revyver.com>
2013-11-17 22:13:52 -0800

    Test the call_attribute utility method.

---
+++ tests/correlations/test_utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import datetime
+import pytest
+
+from components.correlations.utils import call_attributes
+
+FIELDS = ['started']
+
+
+def test_successful_attribute_call():
+    class ProperlyAttributed(object):
+        started = datetime.datetime.now()
+
+    obj = ProperlyAttributed()
+    assert call_attributes(obj, FIELDS)
+
+
+def test_failing_attribute_call():
+    class ImproperlyAttributed(object):
+        timestamp = datetime.datetime.now()
+
+    obj = ImproperlyAttributed()
+    with pytest.raises(AttributeError):
+        call_attributes(obj, FIELDS)
