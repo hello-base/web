441fa54a94de56b865aa2cf2ac56ba6e5ade15aa
Bryan Veloso <bryan@revyver.com>
2013-11-19 14:17:18 -0800

    Add an admin panel for correlations.

---
+++ base/components/correlations/admin.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import Correlation
+
+
+class CorrelationAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
+    fieldsets = (
+        ('Subject', {'fields': (('content_type', 'identifier'), 'object_id')}),
+        ('Correlation Details', {'fields': ('timestamp', 'description')}),
+        ('Derived Data', {
+            'classes': ('grp-collapse grp-open',),
+            'fields': (('year', 'month', 'day'), 'julian')
+        })
+    )
+    list_display = ['content_object', 'content_type', 'timestamp', 'date_field',]
+    list_filter = ['content_type', 'date_field']
+
+    autocomplete_lookup_fields = {'generic': [['content_type', 'object_id']]}
+    readonly_fields = ['identifier', 'year', 'month', 'day', 'julian']
+admin.site.register(Correlation, CorrelationAdmin)
