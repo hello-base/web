f081f180ce04b4f8b160cda931415190eb3dc5d3
Bryan Veloso <bryan@revyver.com>
2013-11-21 12:24:19 -0800

    Don't pollute the testing settings.

--- base/settings/development.py
+++ base/settings/development.py
@@ -77,3 +77,9 @@ class Development(Settings):
             'INDEX_NAME': 'haystack',
         },
     }
+
+    # django-imagekit.
+    # --------------------------------------------------------------------------
+    IMAGEKIT_CACHEFILE_DIR = 'cache'
+    IMAGEKIT_DEFAULT_CACHEFILE_STRATEGY = 'imagekit.cachefiles.strategies.Optimistic'
+    IMAGEKIT_SPEC_CACHEFILE_NAMER = 'imagekit.cachefiles.namers.source_name_dot_hash'
