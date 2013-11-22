bbfcf83db334ba085d8e24ad2cfceea63d073748
Bryan Veloso <bryan@revyver.com>
2013-11-22 15:30:44 -0800

    Correct the tasks.

--- base/components/social/youtube/tasks.py
+++ base/components/social/youtube/tasks.py
@@ -8,10 +8,9 @@ def fetch_all_videos(username):
     # Connect to the API and grab the feed.
     channel = Channel.objects.get(username=username)
     entries = channel.entries()
-    for video in entries:
+    for ytid in entries:
         # YouTube IDs will stay at 11 characters for the distant
         # future, so this should be a safe way to grab the ID.
-        ytid = video.id.text[-11:]
         obj, created = Video.objects.get_or_create(channel=channel, ytid=ytid)
         obj.save()
 
@@ -21,9 +20,8 @@ def fetch_latest_videos():
     for channel in Channel.objects.all():
         # Connect to the API and grab the feed.
         entries = channel.latest_entries()
-        for video in entries.entry:
+        for ytid in entries.entry:
             # YouTube IDs will stay at 11 characters for the distant
             # future, so this should be a safe way to grab the ID.
-            ytid = video.id.text[-11:]
             obj, created = Video.objects.get_or_create(channel=channel, ytid=ytid)
             obj.save()
