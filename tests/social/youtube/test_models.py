f8e474d6dc87c71735e24aa8ad80477247303db7
Bryan Veloso <bryan@revyver.com>
2013-11-23 01:21:47 -0800

    Change le tests, they pass again.

--- tests/social/youtube/test_models.py
+++ tests/social/youtube/test_models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import gdata.youtube
 import pytest
 
 from components.social.youtube.models import Channel, Thumbnail, Video
@@ -14,43 +13,27 @@ class TestChannels:
         factory = ChannelFactory()
         assert isinstance(factory, Channel)
 
-    def test_fetch_all_videos(self):
-        channel = ChannelFactory(username='revyver')
-        entries = channel.entries()
-        assert len(entries) > 0
-        for entry in entries:
-            assert isinstance(entry, gdata.youtube.YouTubeVideoEntry)
-
-    def test_fetch_latest_videos(self):
-        channel = ChannelFactory(username='revyver')
-        entries = channel.latest_entries()
-        assert isinstance(entries, gdata.youtube.YouTubeVideoFeed)
-        assert len(entries.entry) == 10
-
 
 class TestVideos:
-    def test_factory(self):
-        factory = VideoFactory(ytid='MhH_ucrPMZc')
-        assert isinstance(factory, Video)
-        assert 'Morning Musume' in factory.title
-        assert factory.thumbnails.count() > 0
-        for thumbnail in factory.thumbnails.all():
+    @pytest.fixture
+    def video(self):
+        return VideoFactory(ytid='lmXS7cypzCI')
+
+    def test_factory(self, video):
+        assert isinstance(video, Video)
+        assert 'Hello! Party' in video.title
+        assert video.thumbnails.count() > 0
+        for thumbnail in video.thumbnails.all():
             assert isinstance(thumbnail, Thumbnail)
 
-    def test_get_absolute_url(self):
-        url = 'https://www.youtube.com/watch?v=MhH_ucrPMZc&feature=youtube_gdata_player'
-        factory = VideoFactory(ytid='MhH_ucrPMZc', watch_url=url)
-        assert factory.get_absolute_url() == url
+    def test_get_absolute_url(self, video):
+        url = 'http://youtu.be/lmXS7cypzCI'
+        assert video.get_absolute_url() == url
 
-    def test_duration_display(self):
-        factory = VideoFactory(ytid='MhH_ucrPMZc')
-        assert factory.duration_display() == '0:04:20'
-
-    def test_fetch_video(self):
-        video = VideoFactory(ytid='MhH_ucrPMZc')
+    def test_entry(self, video):
         entry = video.entry()
-        assert isinstance(entry, gdata.youtube.YouTubeVideoEntry)
-        assert 'Morning Musume' in entry.title.text
+        assert 'Hello! Party' in entry['snippet']['title']
+        assert 'PT1M27S' in entry['contentDetails']['duration']
 
 
 class TestThumbnails:
