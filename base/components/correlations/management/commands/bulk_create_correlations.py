b9837ac8c25ab678d7af70499f9ca5c0232ca4c5
Bryan Veloso <bryan@revyver.com>
2013-12-05 22:43:43 -0800

    Create a script to backfill our correlations.

---
+++ base/components/correlations/management/commands/bulk_create_correlations.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from itertools import chain
+
+from django.core.management.base import NoArgsCommand
+from django.db.models.signals import pre_save
+
+from components.correlations.models import record_correlation
+from components.people.models import Group, Idol, Membership
+from components.merchandise.music.models import Album, Single
+
+QUERYSETS = [
+    # components.people
+    Group.objects.all(),
+    Idol.objects.all(),
+    Membership.objects.all(),
+
+    # components.music
+    Album.objects.all(),
+    Single.objects.all(),
+]
+
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+        # Manually create the correlations! Don't depend on the save() method
+        # since that opens whole cans of worms that we don't want.
+        for queryset in QUERYSETS:
+            for obj in queryset:
+                record_correlation(type(obj), obj)
