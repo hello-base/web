313b60b6e5c6f0c3e82ab4f890306547a7810752
Bryan Veloso <bryan@revyver.com>
2013-12-06 17:38:03 -0800

    Create a mapping to change date field names into something readable.

--- base/components/correlations/constants.py
+++ base/components/correlations/constants.py
@@ -16,3 +16,18 @@ SUBJECTS = [
 ]
 MODELS = set(s[0] for s in SUBJECTS)
 FIELDS = set(s[1] for s in SUBJECTS)
+
+FIELD_MAPPING = {
+    ('music', 'album', 'released'): 'new albums',
+    ('music', 'single', 'released'): 'new singles',
+    ('people', 'group', 'ended'): 'disbanded groups',
+    ('people', 'group', 'started'): 'new groups',
+    ('people', 'idol', 'birthdate'): 'born',
+    ('people', 'idol', 'graduated'): 'graduations',
+    ('people', 'idol', 'retired'): 'retirements',
+    ('people', 'idol', 'started'): 'new idols',
+    ('people', 'membership', 'ended'): 'left groups',
+    ('people', 'membership', 'leadership_ended'): 'new leaders',
+    ('people', 'membership', 'leadership_started'): 'ex-leaders',
+    ('people', 'membership', 'started'): 'joined groups',
+}
--- base/components/correlations/templatetags/correlation_tags.py
+++ base/components/correlations/templatetags/correlation_tags.py
@@ -4,6 +4,8 @@ import calendar
 from django import template
 register = template.Library()
 
+from components.correlations.constants import FIELD_MAPPING
+
 
 @register.inclusion_tag('correlations/partials/history_list_item.html')
 def render_correlation(instance):
@@ -18,3 +20,9 @@ def render_correlation(instance):
 @register.filter
 def month_name(month_number):
     return calendar.month_name[month_number]
+
+
+@register.simple_tag
+def render_statistic_label(app_label, model_name, date_field):
+    key = (app_label, model_name, date_field)
+    return FIELD_MAPPING[key]
