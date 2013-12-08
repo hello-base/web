3c0904e4f0fa0ab3b9a1cd5d97465ce64acee855
Bryan Veloso <bryan@revyver.com>
2013-12-08 02:13:35 -0800

    Give me some "this day" styling.

--- base/components/views.py
+++ base/components/views.py
@@ -34,7 +34,9 @@ class SiteView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(SiteView, self).get_context_data(**kwargs)
+        correlations = Correlation.objects.exclude(identifier='idol')[:8]
         context['happenings'] = self.get_happenings()
+        context['onthisday'] = Correlation.objects.today()
         context['counts'] = self.get_counts()
         return context
 
