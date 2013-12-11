3d2e21591bdaa25ae81957e13c2c339ca47f0aa0
Bryan Veloso <bryan@revyver.com>
2013-12-11 14:54:24 -0800

    Get Celery working.

---
+++ base/celery.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+from configurations import importer
+from dotenv import read_dotenv
+
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
+
+# Read our `.env` for development purposes (which is now a directory up),
+# then initialize django-configurations.
+read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+importer.install()
+
+# Initialize Celery and tell it we have some apps.
+app = Celery('base')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
