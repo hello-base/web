68c765e328369ae124f031f664e33aa458d2e48c
Bryan Veloso <bryan@revyver.com>
2013-12-24 16:20:22 -0800

    Flake dat.

--- tests/correlations/test_templatetags.py
+++ tests/correlations/test_templatetags.py
@@ -1,12 +1,1 @@
 # -*- coding: utf-8 -*-
-import datetime
-import pytest
-
-from bs4 import BeautifulSoup
-
-from django.template import Template, Context
-
-from components.correlations.models import Correlation
-from components.people.factories import GroupFactory
-
-
