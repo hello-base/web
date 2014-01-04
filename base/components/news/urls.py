3e349824aa75e7fb1c761c2caf4e6d5a4e43367a
Bryan Veloso <bryan@revyver.com>
2014-01-04 14:31:21 -0800

    Basic views and URL confs for news.

--- base/components/news/urls.py
+++ base/components/news/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 
-from .views import NewsIndexView
+from .views import ItemDetailView, NewsIndexView
 
 
 urlpatterns = patterns('',
+    url(r'^news/(?P<year>\d+)/(?P<month>[-\w]+)/(?P<slug>[-\w]+)/$', view=ItemDetailView.as_view(), name='item-detail'),
     url(r'^news/$', view=NewsIndexView.as_view(), name='news-index'),
 )
--- base/components/news/views.py
+++ base/components/news/views.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.dates import DateDetailView, YearArchiveView
 
+from .models import Item
+
 
 class NewsIndexView(YearArchiveView):
-    pass
+    model = Item
 
 
 class ItemDetailView(DateDetailView):
-    pass
+    model = Item
+    date_field = 'published'
