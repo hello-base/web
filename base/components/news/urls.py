120d75dd91bc42778a68c762357feaa762b2800f
Bryan Veloso <bryan@revyver.com>
2014-01-05 14:45:31 -0800

    Modify the year regex.

--- base/components/news/urls.py
+++ base/components/news/urls.py
@@ -5,6 +5,6 @@ from .views import ItemDetailView, NewsIndexView
 
 
 urlpatterns = patterns('',
-    url(r'^news/(?P<year>\d+)/(?P<month>[-\w]+)/(?P<slug>[-\w]+)/$', view=ItemDetailView.as_view(), name='item-detail'),
+    url(r'^news/(?P<year>\d{4})/(?P<month>[-\w]+)/(?P<slug>[-\w]+)/$', view=ItemDetailView.as_view(), name='item-detail'),
     url(r'^news/$', view=NewsIndexView.as_view(), name='news-index'),
 )
