66352bed2969a6fdef96468accfdc4e646bbaeb6
Bryan Veloso <bryan@revyver.com>
2014-01-05 14:32:49 -0800

    Use the right view.

--- base/components/news/views.py
+++ base/components/news/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.views.generic.dates import DateDetailView, YearArchiveView
+from django.views.generic.dates import ArchiveIndexView, DateDetailView
 
 from .models import Item
 
 
-class NewsIndexView(YearArchiveView):
+class NewsIndexView(ArchiveIndexView):
     model = Item
+    date_field = 'published'
 
 
 class ItemDetailView(DateDetailView):
