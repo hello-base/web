eba5f10f658624041c93601d9de6e63288247ad1
Bryan Veloso <bryan@revyver.com>
2014-01-05 17:31:50 -0800

    Sorcery. DRY. Facts.

--- base/components/facts/models.py
+++ base/components/facts/models.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
 from components.events.models import Event, Performance
@@ -6,22 +8,29 @@ from components.merchandise.music.models import Album, Single
 from components.people.models import Idol, Group
 
 
+SUBJECTS = [
+    Idol, Group,        # people
+    Album, Single,      # merchandise.music
+    Event, Performance  # events
+]
+
 class Fact(models.Model):
     body = models.TextField()
 
-    # Subjects.
-    album = models.ForeignKey(Album, blank=True, null=True, related_name='%(class)ss')
-    group = models.ForeignKey(Group, blank=True, null=True, related_name='%(class)ss')
-    idol = models.ForeignKey(Idol, blank=True, null=True, related_name='%(class)ss')
-    single = models.ForeignKey(Single, blank=True, null=True, related_name='%(class)ss')
-    event = models.ForeignKey(Event, blank=True, null=True, related_name='%(class)ss')
-    performance = models.ForeignKey(Performance, blank=True, null=True, related_name='%(class)ss')
-
     def __unicode__(self):
-        return u'%s: %s...' % (self.parent.romanized_name, self.body[:40])
+        return '%s: %s...' % (self.parent.romanized_name, self.body[:40])
 
     @property
     def parent(self):
-        return filter(None, [
-            self.idol, self.group, self.album, self.single, self.event, self.performance]
-        )[0]
+        return filter(None, [getattr(self, subject._meta.model_name) for subject in SUBJECTS])[0]
+
+
+# Subjects.
+# In an everlasting effort to not repeat thyself, we create the subject
+# foreign keys dynamically. In order to do that, we have to do it outside
+# of the initial model specification.
+for subject in SUBJECTS:
+    Fact.add_to_class(
+        subject._meta.model_name,
+        models.ForeignKey(subject, blank=True, null=True, related_name='%(class)ss')
+    )
