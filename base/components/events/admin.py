73ee15cac9d3fa3fb5c56c13813ceda2c4fe4937
Jen Veloso <jen@revyver.com>
2014-01-17 11:46:25 -0800

    Removed "secondary_info_link" from event admin
    Gonna be separated by comma from now on.

--- base/components/events/admin.py
+++ base/components/events/admin.py
@@ -43,8 +43,8 @@ class EventAdmin(ContributorMixin, admin.ModelAdmin):
             'description': 'This is calculated by the values inputted in "Participants."',
             'fields': ('participating_idols', 'participating_groups')
         }),
-        ('Links', {'fields': ('info_link', 'secondary_info_link')}),
         ('Imagery', {'fields': ('logo', 'poster', 'stage')}),
+        ('Links', {'fields': ('info_link_name', 'info_link')}),
     )
     inlines = [PerformanceInline, FactInline, SummaryInline]
     list_display = ['romanized_name', 'name', 'nickname', 'start_date', 'end_date']
