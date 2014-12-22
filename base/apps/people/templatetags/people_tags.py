c4c27673ef23a3f1479d3b20bf8f621a7fd90435
Bryan Veloso <bryan@revyver.com>
2014-12-22 11:35:21 -0800

    Implement designations with a template tag, etc.

--- base/apps/people/templatetags/people_tags.py
+++ base/apps/people/templatetags/people_tags.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+
 from django import template
 register = template.Library()
 
+from ..models import Group
 from ..utils import calculate_age
 
 
 @register.filter
 def age(value, target=None):
     return calculate_age(value, target=target)
+
+
+@register.simple_tag
+def designation_for(target, target_date=date.today()):
+    if isinstance(target, Group):
+        return target.designation_for(target=target_date)
+    return target.romanized_name
