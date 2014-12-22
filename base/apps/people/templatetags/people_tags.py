2388dac21e7a8c40425b78587ab3369929336e1a
Bryan Veloso <bryan@revyver.com>
2014-12-22 11:42:35 -0800

    Distinguish between romanized designations and Japanese ones.

--- base/apps/people/templatetags/people_tags.py
+++ base/apps/people/templatetags/people_tags.py
@@ -16,5 +16,12 @@ def age(value, target=None):
 @register.simple_tag
 def designation_for(target, target_date=date.today()):
     if isinstance(target, Group):
-        return target.designation_for(target=target_date)
+        return target.designation_for(target=target_date).name
+    return target.name
+
+
+@register.simple_tag
+def romanized_designation_for(target, target_date=date.today()):
+    if isinstance(target, Group):
+        return target.designation_for(target=target_date).romanized_name
     return target.romanized_name
