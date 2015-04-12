5b7d56aedadb393ccc9ff92246902c1effa00564
Bryan Veloso <bryan@avalonstar.com>
2015-04-12 16:35:46 -0700

    Yet another base class.

--- base/apps/merchandise/music/search_indexes.py
+++ base/apps/merchandise/music/search_indexes.py
@@ -4,31 +4,23 @@ from haystack import indexes
 from .models import Album, Single
 
 
-class AlbumIndex(CelerySearchIndex, indexes.Indexable):
+class ReleaseIndex(CelerySearchIndex, indexes.Indexable)
     text = indexes.NgramField(document=True, use_template=True)
     model = indexes.CharField(model_attr='_meta__verbose_name_plural', faceted=True)
     romanized_name = indexes.CharField(model_attr='romanized_name')
     name = indexes.CharField(model_attr='name')
 
+    def index_queryset(self, using=None):
+        qs = super(ReleaseIndex, self).index_queryset(using=using)
+        qs = qs.prefetch_related('participating_idols', 'participating_groups')
+        return qs
+
+
+class AlbumIndex(ReleaseIndex):
     def get_model(self):
         return Album
 
-    def index_queryset(self, using=None):
-        qs = super(AlbumIndex, self).index_queryset(using=using)
-        qs = qs.prefetch_related('participating_idols', 'participating_groups')
-        return qs
 
-
-class SingleIndex(CelerySearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
-    model = indexes.CharField(model_attr='_meta__verbose_name_plural', faceted=True)
-    romanized_name = indexes.CharField(model_attr='romanized_name')
-    name = indexes.CharField(model_attr='name')
-
+class SingleIndex(ReleaseIndex):
     def get_model(self):
         return Single
-
-    def index_queryset(self, using=None):
-        qs = super(SingleIndex, self).index_queryset(using=using)
-        qs = qs.prefetch_related('participating_idols', 'participating_groups')
-        return qs
